216743205434ce466856988decc8c0a1a354abaf
jries <ries@embl.de>
2018-11-22 18:41:26 +0100

    relative power

--- shared/myfunctions/makerelativetopwr.m
+++ shared/myfunctions/makerelativetopwr.m
@@ -1,6 +1,6 @@
 function p=makerelativetopwr(p)
 smapdir=pwd;
-if strcmp(p(1:length(smapdir)),smapdir)
+if strcmp(p(1:min(length(smapdir),length(p))),smapdir)
     p=p(length(smapdir)+1:end);
 
     if strcmp(p(1),filesep)
