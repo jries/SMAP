8e84309c48b576c2b97a34fd83555d7affb34779
ries <jonas.ries@gmail.com>
2019-03-20 14:51:55 +0100

    tooltips for simulate sites

--- plugins/+ROIManager/+Segment/SimulateSites.m
+++ plugins/+ROIManager/+Segment/SimulateSites.m
@@ -12,6 +12,7 @@ classdef SimulateSites<interfaces.DialogProcessor&interfaces.SEProcessor
                 obj@interfaces.DialogProcessor(varargin{:});
             obj.inputParameters={'se_sitefov','se_cellpixelsize','se_siteroi'};
             obj.history=true;
+            obj.guiselector.show=true;
         end
         function initGui(obj)
             setvisibility(obj);
@@ -184,18 +185,22 @@ pard.tif_imagesize.Width=0.5;
 pard.t_labelingefficiency.object=struct('String','Labeling efficiency','Style','text');
 pard.t_labelingefficiency.position=[3,1];
 pard.t_labelingefficiency.Width=1.5;
+pard.t_labelingefficiency.TooltipString=sprintf('fraction of target molecules that carry a fluorophore');
 
 pard.labeling_efficiency.object=struct('String','.5','Style','edit');
 pard.labeling_efficiency.Width=.5;
 pard.labeling_efficiency.position=[3,2.5];
+pard.labeling_efficiency.TooltipString=pard.t_labelingefficiency.TooltipString;
 
 pard.modelt.object=struct('String','Model:','Style','text');
 pard.modelt.Width=.5;
 pard.modelt.position=[4,1];
+pard.modelt.TooltipString=sprintf('Select model for blinking: \n simple: re-activations appear randomly in any frame \n PAFP: re-activations appear shortly after previous activation \n Dye: if a dye is not bleached, a re-activation occurs a random time point after the previous one (exponential distribution)');
 
 pard.model.object=struct('String',{{'simple','PAFP','Dye'}},'Style','popupmenu');
 pard.model.Width=.75;
 pard.model.position=[4,1.5];
+pard.model.TooltipString=pard.modelt.TooltipString;
 
 pard.t2.object=struct('String','re-activations','Style','text');
 pard.t2.position=[4,2.25];
@@ -204,7 +209,8 @@ pard.t2.Width=.75;
 pard.blinks.object=struct('String','1','Style','edit');
 pard.blinks.Width=.5;
 pard.blinks.position=[4,3];
-
+pard.blinks.TooltipString=sprintf('Number of re-activations. Zero means: only one actvation per fluorophore');
+pard.t2.TooltipString=pard.blinks.TooltipString;
 
 pard.t3.object=struct('String','lifetime (fr)','Style','text');
 pard.t3.position=[5,3.75];
@@ -213,6 +219,8 @@ pard.t3.Width=0.75;
 pard.lifetime.object=struct('String','1','Style','edit');
 pard.lifetime.Width=.5;
 pard.lifetime.position=[5,4.5];
+pard.lifetime.TooltipString=sprintf('average on-time (in frames) of an activated fluorophore');
+pard.t3.TooltipString=pard.lifetime.TooltipString;
 
 pard.t4.object=struct('String','mean photons','Style','text');
 pard.t4.position=[5,1];
@@ -221,6 +229,8 @@ pard.t4.Width=.75;
 pard.photons.object=struct('String','2000','Style','edit');
 pard.photons.Width=.5;
 pard.photons.position=[5,1.75];
+pard.photons.TooltipString=sprintf('mean number of photons emitted by an activated fluorophore before off-switching \n (this number is distributed among the frames the fluorophore is on).');
+pard.t4.TooltipString=pard.photons.TooltipString;
 
 pard.t5.object=struct('String','BG/pixel','Style','text');
 pard.t5.position=[5,2.5];
@@ -229,11 +239,18 @@ pard.t5.Width=0.75;
 pard.background.object=struct('String','20','Style','edit');
 pard.background.Width=.5;
 pard.background.position=[5,3.];
-
+pard.background.TooltipString=sprintf('Background in photons/pixel/frame');
+pard.t5.TooltipString=pard.background.TooltipString;
 
 pard.t6.object=struct('String','Number of sites','Style','text');
 pard.t6.position=[8,1];
 pard.t6.Width=1.5;
+
+pard.numberofsites.object=struct('String','6 5','Style','edit');
+pard.numberofsites.Width=.5;
+pard.numberofsites.position=[8,2.5];
+pard.numberofsites.TooltipString=sprintf('Number of simulated structures. Pass on two numbers [M N] to generate a grid of MxN structures.');
+pard.t6.TooltipString=pard.numberofsites.TooltipString;
 
 pard.randomrot.object=struct('String','Random rot theta (deg):','Style','checkbox');
 pard.randomrot.Width=1.75;
@@ -242,23 +259,24 @@ pard.randomrot.position=[7,1];
 pard.randomrotangle.object=struct('String','15','Style','edit');
 pard.randomrotangle.Width=.5;
 pard.randomrotangle.position=[7,2.5];
+pard.randomrot.TooltipString=sprintf('randomly rotate the structures by a maximum angle (degree) defined here. \n The structures are rotated additionally by a random angle around the z-axis.');
+pard.randomrotangle.TooltipString=pard.randomrot.TooltipString;
 
 pard.randomxy.object=struct('String','Random position (nm):','Style','checkbox');
 pard.randomxy.Width=1.5;
 pard.randomxy.position=[7,3];
+pard.randomxy.TooltipString=sprintf('Structures are displaced in x,y,z by a random distance');
 
 pard.randomxyd.object=struct('String','20','Style','edit');
 pard.randomxyd.Width=.5;
 pard.randomxyd.position=[7,4.5];
+pard.randomxyd.TooltipString=pard.randomxy.TooltipString;
 
 pard.savez.object=struct('String','save z','Style','checkbox','Value',1);
 pard.savez.Width=1;
 pard.savez.position=[2,3];
-
-pard.numberofsites.object=struct('String','6 5','Style','edit');
-pard.numberofsites.Width=.5;
-pard.numberofsites.position=[8,2.5];
-
+pard.savez.TooltipString=sprintf('Also simulate z-coordinate');
+pard.savez.Optional=true;
 
 pard.t7.object=struct('String','number of frames','Style','text');
 pard.t7.position=[8,3];
@@ -267,10 +285,13 @@ pard.t7.Width=1.5;
 pard.maxframes.object=struct('String','100000','Style','edit');
 pard.maxframes.Width=.5;
 pard.maxframes.position=[8,4.5];
+pard.maxframes.TooltipString=sprintf('Maximum number of frames that contain all localizations');
+pard.t7.TooltipString=pard.maxframes.TooltipString;
 
 pard.savenow.object=struct('String',{{'No saving','Save ground truth','Save simulated locs'}},'Style','popupmenu');
 pard.savenow.Width=2;
 pard.savenow.position=[2,1];
+pard.savenow.Optional=true;
 pard.plugininfo.type='ROI_Analyze';
 
 pard.plugininfo.description=sprintf(['SimulateSites is a localization based simulation engine for SMAP. It \n'...
