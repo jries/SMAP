feae4677231d6e2c8b2d5c4ec47d1adb7b2ac744
Yu-Le Wu <yu-le.wu@embl.de>
2019-05-06 15:08:50 +0200

    CMEpro: some modifications in the cell boundary viewer

--- plugins/+ROIManager/+Evaluate/CMEeqViewCellBound.m
+++ plugins/+ROIManager/+Evaluate/CMEeqViewCellBound.m
@@ -9,7 +9,7 @@ classdef CMEeqViewCellBound<interfaces.SEEvaluationProcessor
         function out=run(obj,p)
             out = runCMEeqViewCellBound(obj,p);
         end
-        function pard=guidef
+        function pard=guidef(obj)
             pard=guidef;
         end
     end
@@ -17,14 +17,14 @@ classdef CMEeqViewCellBound<interfaces.SEEvaluationProcessor
 end
 
 function pard=guidef
-pard.boun.object=struct('Style','checkbox','String','Show boundary');
-pard.boun.position=[1,1];
-pard.boun.Width=2;
-pard.tan.object=struct('Style','checkbox','String','Show tangentLine');
-pard.tan.position=[2,1];
-pard.tan.Width=2;
+    pard.boun.object=struct('Style','checkbox','String','Show boundary');
+    pard.boun.position=[1,1];
+    pard.boun.Width=2;
+    pard.tan.object=struct('Style','checkbox','String','Show tangentLine');
+    pard.tan.position=[2,1];
+    pard.tan.Width=2;
 
-% pard.dxt.Width=3;
-pard.inputParameters={'numberOfLayers','sr_layerson','se_cellfov','se_sitefov','se_siteroi'};
-pard.plugininfo.type='ROI_Evaluate';
+    % pard.dxt.Width=3;
+    pard.inputParameters={'numberOfLayers','sr_layerson','se_cellfov','se_sitefov','se_siteroi'};
+    pard.plugininfo.type='ROI_Evaluate';
 end
