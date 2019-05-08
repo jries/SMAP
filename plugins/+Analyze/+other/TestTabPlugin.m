0aefe82d84d71415d1d19bbf275a989880fa7444
Jonas Ries <jonasries@mac-ries20.pool.embl.de>
2019-05-08 09:47:52 +0200

    tabs in plugins work

--- plugins/+Analyze/+other/TestTabPlugin.m
+++ plugins/+Analyze/+other/TestTabPlugin.m
@@ -1,9 +1,5 @@
 classdef TestTabPlugin<interfaces.DialogProcessor
-%  pair correlation functions calculated according to:
-%  Sengupta, Prabuddha, Tijana Jovanovic-Talisman, Dunja Skoko, Malte Renz, 
-%  Sarah L Veatch, and Jennifer Lippincott-Schwartz. “Probing Protein Heterogeneity 
-%  in the Plasma Membrane Using PALM and Pair Correlation Analysis.” 
-%  Nature Methods 8 (September 18, 2011): 969.
+% study how to use tabs in module interfaces. 
     properties
         
     end
@@ -12,19 +8,23 @@ classdef TestTabPlugin<interfaces.DialogProcessor
             obj@interfaces.DialogProcessor(varargin{:}) ;
         end
         function makeGui(obj,varargin)
-             Vrimold=obj.guiPar.Vrim;
-             makeGui@interfaces.DialogProcessor(obj,varargin{:});
-    
-            obj.guiPar.Vrim=50;
-            obj.guihandles.tg=uitabgroup(obj.handle,'Position',[0 0 1 .75],'SelectionChangedFcn',{@selectLayer_callback,obj});
-            handleold=obj.handle;
-            obj.guihandles.tab1=uitab(obj.guihandles.tg,'Title','Main');
-            obj.handle=obj.guihandles.tab1;
-            makeGui@interfaces.GuiModuleInterface(obj,guidef1);
-            obj.guihandles.tab2=uitab(obj.guihandles.tg,'Title','Module 1');
-            obj.handle=obj.guihandles.tab2;
-            makeGui@interfaces.GuiModuleInterface(obj,guidef2);
-            obj.guihandles.tab3=uitab(obj.guihandles.tg,'Title','+');
+            Vrimold=obj.guiPar.Vrim;handleold=obj.handle;
+           
+            if nargin >2 && varargin{2}==true
+                 obj.guiPar.Vrim=50;
+                 makeGui@interfaces.GuiModuleInterface(obj,varargin{1});
+            else
+                makeGui@interfaces.DialogProcessor(obj,varargin{:});
+                obj.guiPar.Vrim=50;
+                obj.guihandles.tabgroup=uitabgroup(obj.handle,'Position',[0 0 1 .75],'SelectionChangedFcn',{@selectLayer_callback,obj});
+                obj.guihandles.tab1=uitab(obj.guihandles.tabgroup,'Title','Main');
+                obj.handle=obj.guihandles.tab1;
+                makeGui@interfaces.GuiModuleInterface(obj,guidef1);
+                
+                obj.addguitotab(1)
+                
+                obj.guihandles.tab3=uitab(obj.guihandles.tabgroup,'Title','+');
+            end
             obj.handle=handleold;
             obj.guiPar.Vrim=Vrimold;
         end
@@ -36,42 +36,42 @@ classdef TestTabPlugin<interfaces.DialogProcessor
             pard.plugininfo.description= 'x';
             pard.plugininfo.type='ProcessorPlugin';
         end
-%         function pard=guidef2(obj)
-%             pard=guidef2;
-%         end
-
+        function addguitotab(obj,number)
+            tag=['Module' num2str(number)];
+            obj.guihandles.(['tab' num2str(number)])=uitab(obj.guihandles.tabgroup,'Title',tag,'Tag',tag);
+            guidefhere=addnumbertofield(guidefmodule,number);
+            Vrimold=obj.guiPar.Vrim;handleold=obj.handle;
+            obj.guiPar.Vrim=50;
+            obj.handle=obj.guihandles.(['tab' num2str(number)]);
+            obj.makeGui(guidefhere,1);
+            obj.handle=handleold;
+            obj.guiPar.Vrim=Vrimold;
+        end
     end
 end
 
 
+
 function selectLayer_callback(tabgroup,eventdata,obj)
-layer=(eventdata.NewValue.Tag);
 layertitle=(eventdata.NewValue.Title);
 if strcmp(layertitle,'+')
-    newlayernumber=obj.numberOfLayers+1;
-    tag=['Layer' num2str(newlayernumber)];
-      h.(['tab_layer' num2str(newlayernumber)])=uitab(obj.guihandles.layertab,'Title',tag,'Tag',tag);
-       h.(['layer_' num2str(newlayernumber)])=obj.addlayer(  h.(['tab_layer' num2str(newlayernumber)]),newlayernumber);
-    obj.numberOfLayers=newlayernumber;
-    obj.setPar('numberOfLayers',newlayernumber);
+    numtabs=length(tabgroup.Children);
+    obj.addguitotab(numtabs-1)
     s=1:length(tabgroup.Children);
     s(end-1)=s(end);
     s(end)=s(end)-1;
     tabgroup.Children=tabgroup.Children(s);
     tabgroup.SelectedTab=tabgroup.Children(end-1); 
-    selectedlayer=newlayernumber;
-    obj.setPar('layercheck',true,'Value','layer',selectedlayer)
-else
-    selectedlayer=sscanf(layer,'Layer%d');
+
 end
-for k=1:length(tabgroup.Children)-1
-%     layernumber=tabgroup.Children(k).Title;
-    layertag=tabgroup.Children(k).Tag;
-    obj.children.(layertag).setlayer(selectedlayer);
-end
-updatelayernames(obj)
 end
 
+function out=addnumbertofield(in,number)
+fn=fieldnames(in);
+for k=1:length(fn)
+    out.([fn{k} '_' num2str(number)])=in.(fn{k});
+end
+end
 
 function pard=guidef1
 pard.ch1t.object=struct('String','Ch 1','Style','text');
@@ -106,16 +106,12 @@ pard.rpix.Width=.5;
 end
 
 
-function pard=guidef2
-pard.ch1t22.object=struct('String','text','Style','text');
-pard.ch1t22.position=[2,1];
-pard.ch1t22.Width=0.3;
+function pard=guidefmodule
+pard.ch1.object=struct('String','text','Style','text');
+pard.ch1.position=[2,1];
+pard.ch1.Width=0.3;
 
-pard.rrange22.object=struct('String','50','Style','edit');
-pard.rrange22.position=[3,2];
-pard.rrange22.Width=.5;
-
-
-
-
+pard.rrange.object=struct('String','50','Style','edit');
+pard.rrange.position=[3,2];
+pard.rrange.Width=.5;
 end
