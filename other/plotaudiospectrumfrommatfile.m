60c72c4fc64cfaae6055dae5f4b3a7a7d8fb4495
Robin Diekmann <robin.diekmann@embl.de>
2019-05-14 15:21:59 +0200

    plotting of audio spectra includes imagesc

--- other/plotaudiospectrumfrommatfile.m
+++ other/plotaudiospectrumfrommatfile.m
@@ -1,5 +1,5 @@
 clc
-close all
+%close all
 clearvars
 
 plot_frequencies = [43.7 48 48.9 49.4 50 98.1 100];
@@ -15,25 +15,42 @@ for k = 1:length(plot_frequencies)
 end % for k
 
 figure
-subplot(1,2,1)
-    fmin = 30;
+set(gcf, 'Position', [1 55 1440 750])
+
+% plot average spectrum
+subplot(2,2,1)
+    fmin = 85;
     fmax = 105;
     plot(frequency, averagef)
     hold on
-    plot(frequency, min(allf'))
+    plot(frequency, prctile(allf',5))
     plot(frequency, max(allf'))
-    legend('average', 'min', 'max')
+    legend('average', '5th percentile', 'max')
     xlim([fmin fmax]);
     xlabel('Frequency (Hz)')
-    ylabel('Amplitude (a.u.)')
+    ylabel('Amp. (a.u.)')
     title(file)
-
+   
+% plot frequencies over time
 for k = 1:length(plot_frequencies)
     subplot(length(plot_frequencies),2,2*k)
         plot((1:length(allf(ind(k), :)))*10/60, allf(ind(k), :))
         legend(num2str(frequency(ind(k))'))
-        ylabel('Amplitude (norm.)')
+        ylabel('Amp. (a.u.)')
 end % for k
 xlabel('time (minutes)')
 
-set(gcf, 'Position', [1 55 1440 750])
+% 3D plot of frequency range over time
+subplot(2,2,3)
+    ind_min = find( min( abs( frequency - fmin ) ) == abs( frequency - fmin ) );
+    ind_max = find( min( abs( frequency - fmax ) ) == abs( frequency - fmax ) );
+
+    imagesc(allf(ind_min : ind_max, :))
+        xl = get(gca, 'XTick');
+        yl = get(gca, 'YTick');
+        set(gca, 'XTickLabel', cellstr(num2str(round(xl'*10/60))))
+        set(gca, 'YTickLabel', cellstr(num2str(transpose(frequency(ind_min+yl)))))
+
+        xlabel('Time (min)')
+        ylabel('Frequency (Hz)')
+    
