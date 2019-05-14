e132a074c24a533b51466e94704b1f568b733565
Robin Diekmann <robin.diekmann@embl.de>
2019-05-14 10:59:34 +0200

    recording and presentation of audio spectra extended

--- other/audiospectrum.m
+++ other/audiospectrum.m
@@ -1,12 +1,23 @@
+% set audiorecorder to external device if existent
+    audinf = audiodevinfo;
+    if size(audinf.input, 2) > 1
+        audID = audinf.input(2).ID; % get ID from external audio device
+        disp(['Recording from audio device ' audinf.input(2).Name '.'])
+    else
+        audID = audinf.input(1).Name; % get ID from internal audio device
+        disp(['Recording from audio device ' audinf.input(1).Name '.'])
+    end % if
+    
+
 % name='room1';
 name=inputdlg('name / room');
 name=name{1};
-path='/Volumes/t2ries/projects/4Pi/201905_vibrations/';
+path='/Users/diekmann/Documents/24 Audio spectra/';
 
 %record and show power spectrum
 updatetime=10; %s =1/dF
 
-numberOfBlocks=10;
+numberOfBlocks=1700;
 
 fmax=105; %Hz for plotting only
 fmin=30;
@@ -14,7 +25,7 @@ nbits=16;%
 Fs=5000; %sampling frequency
 
 %s
-rec=audiorecorder(Fs,nbits,1);
+rec=audiorecorder(Fs,nbits,1,audID);
 
 figure(88);
 ax=gca;
@@ -32,10 +43,31 @@ for k=1:numberOfBlocks
     plot(ax,f,sp1,f,blockav/k)
     legend(ax,'block','average')
     xlim([fmin fmax]);
+    
+    disp(['Progress: ' num2str(k*updatetime) 's of ' num2str(numberOfBlocks*updatetime) 's'])
 end
+
+figure
+subplot(2,1,1)
+    plot(f,blockav/k, 'Color', [0.8500, 0.3250, 0.0980])
+    legend('average')
+    xlim([fmin fmax]);
+    xlabel('Frequency (Hz)')
+    ylabel('Amplitude (a.u.)')
+    title(name)
+subplot(2,1,2)
+    loglog(f,blockav/k, 'Color', [0.8500, 0.3250, 0.0980])
+    legend('average')
+    %xlim([fmin fmax]);
+    xlabel('Frequency (Hz)')
+    ylabel('Amplitude (a.u.)')
+    title(name)   
+savefig([path 'spec__' name '.fig'])
+    
+    
 averagef=blockav/k;
 frequency=f;
 allf=alltr;
 
-save([path name '.mat'], 'frequency','allf','averagef');
-disp('done')
+save([path 'spec__' name '.mat'], 'frequency','allf','averagef');
+disp('Done')
