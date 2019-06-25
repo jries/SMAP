cb04dceed910cfee2a764785522a258b5adb6c32
Jonas Ries <jonasries@mac-ries20.pool.embl.de>
2019-06-25 10:53:32 +0200

    comment added

--- +gui/private/checkforsingleimages.m
+++ +gui/private/checkforsingleimages.m
@@ -2,7 +2,7 @@ function si=checkforsingleimages(file)
 si=0;
 il=imageLoader(file).info;
 if (strcmp(il.format,'separateTif')||strcmp(il.format,'stackTif'))&&il.numberOfFrames>10
-    button=questdlg('Raw Tif file detected. Localize these images?','Tif loader','Localize Images','Use for rendering','Cancel','Localize Images');
+    button=questdlg('Raw Tif file detected. For localization please load using the [load images] function in the Localize/Input Image tab. Localize these images?','Tif loader','Localize Images','Use for rendering','Cancel','Localize Images');
     if strcmpi(button,'Localize Images')
         si=1;
     elseif strcmpi(button,'Use for reconstruction')
