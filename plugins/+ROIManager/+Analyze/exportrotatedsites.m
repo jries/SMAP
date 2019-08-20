d034360cd4caffaf49a3c116e80b9a2c99d60561
jries <ries@embl.de>
2019-08-20 15:12:10 +0200

    Additional output for ROIManager-Analyze-exportrotatedsites

--- plugins/+ROIManager/+Analyze/exportrotatedsites.m
+++ plugins/+ROIManager/+Analyze/exportrotatedsites.m
@@ -2,12 +2,12 @@ sites=g.locData.SE.sites;
 dy=300; %width of profile
 
 dind=10000;
-out=zeros(dind,7);
+out=zeros(dind,8);
 ind=1;
 for k=1:length(sites)
 
 layers=find(g.getPar('sr_layerson'));
-locs=g.locData.getloc({'xnm','ynm','znm','channel','frame','locprecnm'},'layer',layers,'Position',sites(k));
+locs=g.locData.getloc({'xnm','ynm','znm','channel','frame','locprecnm','clusterdensity'},'layer',layers,'Position',sites(k));
 pos=sites(k).annotation.rotationpos.pos;
 angle=sites(k).annotation.rotationpos.angle;
 len=sites(k).annotation.rotationpos.length;
@@ -25,11 +25,11 @@ if size(out,2)<ind+numlocs
     out(ind+dind,1)=0;
 end
 out(ind:ind+numlocs-1,:)=horzcat(xr(indg),yr(indg),locs.znm(indg),locs.channel(indg),...
-    locs.frame(indg),locs.locprecnm(indg), k+0*xr(indg));
+    locs.frame(indg),locs.locprecnm(indg),locs.clusterdensity(indg), k+0*xr(indg));
 ind=ind+numlocs;
 end
 out(ind:end,:)=[];
-to=array2table(out,'VariableNames',{'xnm','ynm','znm','channel','frame','locprecnm','ID'});
+to=array2table(out,'VariableNames',{'xnm','ynm','znm','channel','frame','locprecnm','clusterdensity','ID'});
 
 outf=[g.locData.files.file(1).name(1:end-8) '_export.mat'];
 [f,path]=uiputfile(outf);
