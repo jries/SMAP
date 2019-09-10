89c52d378b0570cfd6a34d7fb3f10d310336d14e
Jonas Ries <jonasries@mac-ries20.pool.embl.de>
2019-09-10 18:11:30 +0200

    ???

--- plugins/+Analyze/+sr3D/Phase2z4Pi.m
+++ plugins/+Analyze/+sr3D/Phase2z4Pi.m
@@ -49,7 +49,7 @@ classdef Phase2z4Pi<interfaces.DialogProcessor
                 z0all(k)=z0;
             end
             axp=obj.initaxis('phase vs z');
-            z0=getz0phase(zastig(inframe),phase(inframe),frequency,z0,axp);
+%             z0=getz0phase(zastig(inframe),phase(inframe),frequency,z0,axp);
             
             frameposc=framepos(1:end-1)+(framepos(2)-framepos(1))/2;
             z0int=fit(frameposc',z0all','smoothingspline');
