0aa265f4f26b2f6063f3deb39e9a166f0c67c0db
Yiming <yiming.li@embl.de>
2019-11-08 14:07:53 +0100

    Robin: CameraConverter can compute sCMOS maps based on exposure rettime

--- plugins/+WorkflowModules/+Loaders/CameraConverter.m
+++ plugins/+WorkflowModules/+Loaders/CameraConverter.m
@@ -123,7 +123,7 @@ classdef CameraConverter<interfaces.WorkflowModule
            if p.correctcamera %apply offset and brightfield correction
                roi=p.loc_cameraSettings.roi;
                imphot=(single(imgp)-obj.offsetmap(roi(1)+1:roi(1)+roi(3),roi(2)+1:roi(2)+roi(4)))... %*obj.adu2phot...
-                   .*obj.gainmap(roi(1)+1:roi(1)+roi(3),roi(2)+1:roi(2)+roi(4)); %XXX correct or exchanged???
+                   .*obj.gainmap(roi(1)+1:roi(1)+roi(3),roi(2)+1:roi(2)+roi(4)); %XXX correct or exchanged??? a: looks fine
                
            else
                imphot=(single(imgp)-obj.offset)*obj.adu2phot;
@@ -250,14 +250,34 @@ else
 end
 end
 
-function [gainmap,offsetmap,varmap]=makegainoffsetCMOS(l,exposuretime)
-gainmap=l.gainmap;
-offsetmap=l.offsetmap;
-varmap=l.varmap;
+function [gainmap,offsetmap,varmap]=makegainoffsetCMOS(l,exposuretime_data)
+    % if the camera has been characterized exposure time dependent,
+    % find behavior for exposure time used here
+    if isfield(l, 'read_noise_variance') ...
+            & isfield(l, 'thermal_noise_variance_per_s') ...
+            & isfield(l, 'pixel_baseline') ...
+            & isfield(l, 'thermal_counts_per_s')
+        [offsetmap, varmap] = makeExpDependMap(l, exposuretime_data)
+    else
+        offsetmap=l.offsetmap;
+        varmap=l.varmap;
+    end
+    %assume gainmap is independent of exposure time
+        gainmap=l.gainmap;
 
-varmap=varmap.*gainmap.^2; %???? in units of photons, to be used later directly
-%XXXX here make gainmap, offsetmap
+    %varmap has to be in units of photons^2
+        varmap=varmap.*gainmap.^2; 
+            %???? in units of photons, to be used later directly. 
+            %A: Yes, conversion to photons^2 is not considered in GPUmleFIT
 end
+
+function [offsetmap, varmap] = makeExpDependMap(l, exposuretime_data)
+        exposure_time_s = exposuretime_data /1e3; % maps have units seconds, SMAP handles timing in ms
+        
+        varmap = l.read_noise_variance + l.thermal_noise_variance_per_s * exposure_time_s;
+        offsetmap = l.pixel_baseline + l.thermal_counts_per_s * exposure_time_s;
+end % function
+
 
 % function calculategain(img)
 % img=double(img);
