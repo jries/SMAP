7ad26da16bf2b752c54d379cc27ebf5d9f6c5cad
Jonas Ries <jonas.ries@gmail.com>
2020-04-07 19:31:41 +0200

    Ennos script

--- other/profilesEnrico.m
+++ other/profilesEnrico.m
@@ -3,17 +3,20 @@ sites=g.locData.SE.sites;
 pixelsize=g.locData.files.file.info.cam_pixelsize_um;
 tifffile=g.locData.files.file.tif(1).info.name;
 %it got a _1 in the info. strange. Remove that.
-tifffile=[tifffile(1:end-6) '.tif'];
+tifffile=[tifffile(1:end-4) '.tif'];
 tiffreader=mytiffreader(tifffile);
 actinstack=double(tiffreader.readall);
 actinstack=actinstack-myquantilefast(actinstack,0.02); %BG subtraction
 
 tifffile=g.locData.files.file.tif(2).info.name;
 %it got a _1 in the info. strange. Remove that.
-tifffile=[tifffile(1:end-6) '.tif'];
+tifffile=[tifffile(1:end-4) '.tif'];
 tiffreader=mytiffreader(tifffile);
 beadstack=double(tiffreader.readall);
 beadstack=beadstack-myquantilefast(actinstack,0.02);
+
+%overwrite pixel size:
+
 %% Bead movement
 figure(186)
 hold off
@@ -23,7 +26,7 @@ firstframes=3;
 lastframes=100;
 maxframes=max(g.locData.loc.frame);
 for k=length(sites):-1:1
-    locs=g.locData.getloc({'xnm','ynm','frame'},'Position',sites(k),'layer',find(g.getPar('sr_layerson')));
+    locs=g.locData.getloc({'xnm','ynm','frame'},'Position',sites(k),'layer',find(g.getPar('sr_layerson')),'grouping','ungrouped');
     ind=find(locs.frame<=firstframes);
     if isempty(ind)
         ind=1;
@@ -66,14 +69,23 @@ meanstack=mean(stackcut,4);
 fm=figure(187);
 imx(meanstack,'Parent',fm)
 
+%%
+% use displacement to only average over those
 
 %%
+pixelsizeplot=pixelsize(1);
+pixelsizeplot=40;
+
 norm =true;
 timepoints=10;
 df=floor(size(stackcut,3)/timepoints);
 figure(188)
+subplot(2,2,1);hold off;
 col=jet(timepoints+2);col(1:2,:)=[];
-hold off
+
+radius=((0:size(rsum,1)-1)'+0.5)*pixelsizeplot;
+rhd=((0:0.1:size(rsum,1)-1)'+0.5)*pixelsizeplot;
+timeaxis=0:timepoints-1;
 for t=timepoints:-1:1
     for k=length(sites):-1:1
         rsumbin(:,t,k)=mean(rsum(:,(t-1)*df+1:df*t,k),2);
@@ -84,13 +96,33 @@ for t=timepoints:-1:1
         rsummean=rsummean/rsummean(end);
     end
     stackbin(:,:,t)=mean(meanstack(:,:,(t-1)*df+1:df*t),3);
-    
-    plot(rsummean,'Color',col(t,:))
+    subplot(2,2,1)
+    plot(radius,rsummean,'Color',col(t,:))
     hold on
+    %get HD to work
+    rsumhd=interp1(radius,rsummean,rhd,'pchip');
+    [imax(t),ind]=max(rsumhd);
+    rmax(t)=rhd(ind);
 end
+title('intensity profile')
+xlabel('position (nm)')
+ylabel('density (mean intensity)')
+
+subplot(2,2,2)
+plot(timeaxis,rmax);
+title('radius over time')
+xlabel('time')
+ylabel('radius (nm)')
+
+subplot(2,2,3)
+plot(timeaxis,imax);
+
+title('maximum intensity over time')
+xlabel('time')
+ylabel('intensity')
+
 fb=figure(187);
 imx(stackbin,'Parent',fm)
-
 % stackcutb=beadstack(y-roisize:y+roisize,x-roisize:x+roisize,:);
 % function [rs,norm]=callradialsum(img)
 % [rs,norm]=radialsum(img);
