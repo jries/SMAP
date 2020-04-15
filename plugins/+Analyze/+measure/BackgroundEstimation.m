70b083e67d2a2745d19e9ea2d0d770530fa88ad4
Jonas Ries <jonas.ries@gmail.com>
2020-04-15 14:55:32 +0200

    add photon histogram to background plugin

--- plugins/+Analyze/+measure/BackgroundEstimation.m
+++ plugins/+Analyze/+measure/BackgroundEstimation.m
@@ -15,16 +15,26 @@ classdef BackgroundEstimation<interfaces.DialogProcessor
             layers=find(obj.getPar('sr_layerson'));
             for k=1:length(layers)
                 locsu=obj.locData.getloc({'bg','numberInGroup'},'position','roi','layer',layers(k),'grouping','ungrouped');
-                locsg=obj.locData.getloc({'bg','numberInGroup'},'position','roi','layer',layers(k),'grouping','grouped');
+                locsg=obj.locData.getloc({'bg','numberInGroup','phot'},'position','roi','layer',layers(k),'grouping','grouped');
+                locsgall=obj.locData.getloc({'phot'},'position','roi','grouping','grouped');
                 out.bg_ungrouped(k)=mean(locsu.bg);
                 out.bg_grouped_tot(k)=mean(locsg.bg);
                 out.bg_grouped_frame(k)=mean(locsg.bg./locsg.numberInGroup);
+                dphot=10;maxphot=1e5;
+                photrange=0:dphot:maxphot;
+                hc=histcounts(locsg.phot,photrange);
+                hcall=histcounts(locsgall.phot,photrange);
+                photrange(end)=[]; photrange=photrange+dphot/2;
+                out.photonrange=photrange;
+                out.phothistfiltered=hc;
+                out.phothistall=hcall;
             end
 
         end
         function pard=guidef(obj)
             pard.text.object=struct('String','calculate background', 'Style','text');
             pard.text.position=[1,1];
+            pard.text.Width=4;
             pard.plugininfo.type='ProcessorPlugin';
             pard.plugininfo.description='Calculates average background per pixel. For the SlowStorm paper';
          
