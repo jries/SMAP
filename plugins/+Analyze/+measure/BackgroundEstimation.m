7d0585f210fe4361ed538831139b828ea9aa3b1e
Jonas Ries <jonas.ries@gmail.com>
2020-04-16 09:48:50 +0200

    add photn width and take filters into account

--- plugins/+Analyze/+measure/BackgroundEstimation.m
+++ plugins/+Analyze/+measure/BackgroundEstimation.m
@@ -16,11 +16,11 @@ classdef BackgroundEstimation<interfaces.DialogProcessor
             for k=1:length(layers)
                 locsu=obj.locData.getloc({'bg','numberInGroup'},'position','roi','layer',layers(k),'grouping','ungrouped');
                 locsg=obj.locData.getloc({'bg','numberInGroup','phot'},'position','roi','layer',layers(k),'grouping','grouped');
-                locsgall=obj.locData.getloc({'phot'},'position','roi','grouping','grouped');
+                locsgall=obj.locData.getloc({'phot'},'layer',layers(k),'position','roi','grouping','grouped','removeFilter',{'locprecnm','locprecznm','phot'});
                 out.bg_ungrouped(k)=mean(locsu.bg);
                 out.bg_grouped_tot(k)=mean(locsg.bg);
                 out.bg_grouped_frame(k)=mean(locsg.bg./locsg.numberInGroup);
-                dphot=10;maxphot=1e5;
+                dphot=p.dphot;maxphot=1e5;
                 photrange=0:dphot:maxphot;
                 hc=histcounts(locsg.phot,photrange);
                 hcall=histcounts(locsgall.phot,photrange);
@@ -32,9 +32,17 @@ classdef BackgroundEstimation<interfaces.DialogProcessor
 
         end
         function pard=guidef(obj)
-            pard.text.object=struct('String','calculate background', 'Style','text');
+            pard.text.object=struct('String','calculate background and photon histogram', 'Style','text');
             pard.text.position=[1,1];
             pard.text.Width=4;
+            
+            pard.tphot.object=struct('String','bin width photons:','Style','text');
+            pard.tphot.position=[2,1];
+            pard.tphot.Width=0.5;
+
+            pard.dphot.object=struct('String','100','Style','edit');
+            pard.dphot.position=[2,2.5];
+
             pard.plugininfo.type='ProcessorPlugin';
             pard.plugininfo.description='Calculates average background per pixel. For the SlowStorm paper';
          
