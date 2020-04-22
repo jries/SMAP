314c6a0950cbf16cd86f280fa12febdbd41d0889
Jonas Ries <jonas.ries@gmail.com>
2020-04-22 10:16:38 +0200

    weighting during grouping with w=1/s^2 instead of w=1/s

--- shared/Grouper.m
+++ shared/Grouper.m
@@ -18,21 +18,34 @@ classdef Grouper< interfaces.LocDataInterface
 %             obj.inputParameters={'group_dx','group_dt'};
           %define: meanx, meany, meanz to go with xerr, yerr, zerr when
           %weighting, same for locprecx, locprecy,locprecz.
+          % or not: as averaged locs will have same x,y,z only relative
+          % locprecs count, then it should not matter much which we take or
+          % if we just take 1/sqrt(N). For performance: keep only one
+          % weighting. E.g. locprecnm or N
             obj.combinemodes.xnm='mean';
             obj.combinemodes.ynm='mean';
             obj.combinemodes.znm='mean';
+            obj.combinemodes.xpix='mean';
+            obj.combinemodes.ypix='mean';
+            obj.combinemodes.zpix='mean';
+            obj.combinemodes.znm_SALM='mean';
+            obj.combinemodes.znm_asSALM='mean';
+            obj.combinemodes.znm_a='mean';
             obj.combinemodes.bg='sum';
             obj.combinemodes.bg1='sum';
             obj.combinemodes.bg2='sum';
             obj.combinemodes.phot='sum';
             obj.combinemodes.phot1='sum';
             obj.combinemodes.phot2='sum';
-            obj.combinemodes.PSFxnm='square';
-            obj.combinemodes.PSFynm='square';
+            obj.combinemodes.PSFxnm='mean';
+            obj.combinemodes.PSFynm='mean';
+            obj.combinemodes.PSFxpix='mean';
+            obj.combinemodes.PSFypix='mean';
             obj.combinemodes.locprecnm='locp';
             obj.combinemodes.locprecxnm='locp';
             obj.combinemodes.locprecynm='locp';
-            obj.combinemodes.locprecznm='locp';
+            obj.combinemodes.locprecznm='locp';            
+            
             obj.combinemodes.CRLBphot='min';
             obj.combinemodes.frame='min';
             obj.combinemodes.channel='first';
@@ -44,8 +57,8 @@ classdef Grouper< interfaces.LocDataInterface
             obj.combinemodes.filenumber='first';
             obj.combinemodes.neighbours='sum';
             obj.combinemodes.clusterdensity='mean';
-             obj.combinemodes.cellnumbers='max';
-              obj.combinemodes.sitenumbers='max';
+            obj.combinemodes.cellnumbers='max';
+            obj.combinemodes.sitenumbers='max';
         end
         function connect(obj,dx,dt,framef,xf,yf,locpf,varargin)
             fn=fieldnames(obj.locData.loc);
@@ -58,26 +71,18 @@ classdef Grouper< interfaces.LocDataInterface
                     disp([fn{k} ' too short, padded with zeros']);
                     obj.locData.loc.(fn{k})(lm)=obj.locData.loc.(fn{k})(end);
                 end
-            end
-%             obj.status('group localizations')
-            %here not clear. like this or exchanged?
-            
+            end           
             x=double(obj.locData.getloc(xf).(xf));
-            y=double(obj.locData.getloc(yf).(yf));
-            
-             frame=double(obj.locData.getloc(framef).(framef));
-            
+            y=double(obj.locData.getloc(yf).(yf)); 
+            frame=double(obj.locData.getloc(framef).(framef));
             numfields=length(varargin);
-            
-%             sortmatrix=([frame,x,y]);  %modify:unconnected
-%        sorting of y not necessary, as all y are compared.
+            %our algorithm requires localizations sorted by frame, then by
+            %x:
             sortmatrix=zeros(length(frame),numfields+2);
             sortmatrix(:,end-1)=frame;
             sortmatrix(:,end)=x;
-%             sortmatrix(:,end)=y;
             for k=1:length(varargin)
                 sortmatrix(:,k)=double(obj.locData.getloc(varargin{k}).(varargin{k}));
-%                 sortmatrix=[double(obj.locData.getloc(varargin{k}).(varargin{k})),sortmatrix];
             end
             sm=size(sortmatrix);
             [sortmatrixsort,indsort]=sortrows(sortmatrix,1:sm(2));
@@ -88,8 +93,7 @@ classdef Grouper< interfaces.LocDataInterface
                 newgroup=newgroup | diff(sortmatrixsort(:,k));
                 end
             end
-            
-            
+
             %later: remove multiple consequtive 1. For sites of length 1.
             %Always remove the first ones.
             fng=find(newgroup);
@@ -99,15 +103,11 @@ classdef Grouper< interfaces.LocDataInterface
                 end
                 newgroup(fng-1)=0;
                 sortmatrixsort(newgroup,end-1)=0;
-            end
-            
-%             frame(indsort(newgroup))=0;
-            
-            
+            end       
             clear sortmatrix 
-%             
-%             list=connectsingle2c(sortmatrixsort(:,end),double(y(indsort)),sortmatrixsort(:,end-1),double(dx),int32(dt),int32(maxactive));
-           
+            
+            % combine either with fixed search radius, or depending on
+            % loalization precision
             switch obj.mode
                 case 2 %locprec
                     sigmafactor=2.5;
@@ -119,7 +119,7 @@ classdef Grouper< interfaces.LocDataInterface
             end
             clear  sortmatrixsort
             clear frame
-%              listm=connectsingle2mat(double(x(indsort)),double(y(indsort)),double(frame(indsort)),double(dx),int32(dt),int32(maxactive));
+
             if list(end)==0
                 list(end)=max(list)+1; %FIX connectsingle doesnt assign last loc. Fix later!
             end  
@@ -132,65 +132,43 @@ classdef Grouper< interfaces.LocDataInterface
             clear numbers
             [~,indback]=sort(indold);
             listback=list(indback);
-            clear indback;
-          
+            clear indback;         
             obj.locData.setloc('groupindex',listback)
             
              %number of locs
             [listsort,indsort2]=sort(list);
             clear list
-%             numbergroup=zeros(size(listsort));
-%             whos listsort
-            if ~isempty(listsort)
-            numbergroup=countlocs(double(listsort));
-            obj.dllistsort=[diff(listsort);1];
-            clear listsort
-           
-%             groupc=listsort(1);
-%             ng=0;
-%             inds=1;
-%             for k=1:length(numbergroup)
-%                 if listsort(k)~=groupc
-%                     groupc=listsort(k);
-%                     numbergroup(inds:k-1)=ng;
-%                     ng=0;
-%                     inds=k;
-%                 end
-%                 ng=ng+1;
-% %                 numbergroup(k)=ng;
-%             end
-            
-            
-            indold2=indold(indsort2);
-            clear indsort2 indold
-            [~,indback2]=sort(indold2);
-            clear indold2
-            obj.locData.setloc('numberInGroup',single(numbergroup(indback2)));
-            
-            [~,obj.indsortlist]=sort(listback);
-            
-            
-            
-  
-            end
-%             obj.status('group localizations done')
 
-            
+            if ~isempty(listsort)
+                numbergroup=countlocs(double(listsort));
+                obj.dllistsort=[diff(listsort);1];
+                clear listsort          
+
+                indold2=indold(indsort2);
+                clear indsort2 indold
+                [~,indback2]=sort(indold2);
+                clear indold2
+                obj.locData.setloc('numberInGroup',single(numbergroup(indback2)));
+                [~,obj.indsortlist]=sort(listback);           
+            end 
         end
         function combine(obj,field,combinemode,weights,gweights) %no field etc: group everythign for which we have combinemodes
-            
-%             obj.status('group: combine fields')
             if nargin==1 %do all
-%                 fn=fieldnames(obj.combinemodes);
                 fn2=fieldnames(obj.locData.loc);
                 if isempty(obj.locData.loc.(fn2{1}))  
                    return
                 end
-%                 fnall=intersect(fn,fn2);
-                %XXXXX weights should be 1/s2. Wikipedia. not really
-                % also:if xerr, yerr, zerr: use proper ones for weighing.
-                %intuitive...
-                weights=1./(obj.locData.getloc('locprecnm').locprecnm);
+
+                %XXXXX weights should be 1/s2. Wikipedia.
+                % also:if xerr, yerr, zerr: use proper ones for weighing.  
+                if isfield(obj.locData.loc,'locprecnm')
+                     weights=double(1./(obj.locData.getloc('locprecnm').locprecnm).^2); %w=1/s^2
+                elseif isfield(obj.locData.loc,'xnmerr')
+                     weights=double(1./(obj.locData.getloc('xnmerr').xnmerr).^2); %w=1/s^2
+                else                   
+                     weights=double((obj.locData.getloc('phot').phot)); %w=N
+                end
+               
                 if isempty(weights)
                     weights=ones(size(obj.locData.loc.(fn2{1})));  
                 else
@@ -199,116 +177,97 @@ classdef Grouper< interfaces.LocDataInterface
                 
                 list=obj.locData.getloc('groupindex').groupindex;
                 gweights=sumcombineind(double(weights),double(list),double(obj.indsortlist));
-                
                 for k=1:length(fn2)
                     if isfield(obj.combinemodes,fn2{k})
                         combinemode=obj.combinemodes.(fn2{k});
+                    elseif length(fn2{k})>=3 && strcmp(fn2{k}(end-2:end),'err')
+                        combinemode='locp';
                     else
                         combinemode='mean';
                     end
                     obj.combine(fn2{k},combinemode,weights,gweights);
                 end
-                
-            else
-                
-            if nargin>2
-                obj.combinemodes.(field)=combinemode;
-            elseif  nargin==2
-                if isfield(obj.combinemodes,'field')
-                combinemode=obj.combinemodes.(field);
-                else
-                    combinemode='mean';
-                end
-            end
-           
-            vtype=obj.locData.getloc(field).(field)(1);
-            if iscell(vtype)
-                return
-            end
-            v=double(obj.locData.getloc(field).(field));
-          
-            list=obj.locData.getloc('groupindex').groupindex;
-            
-            if nargin <4
-                weights=ones(size(v),'like',v);              
-            end   
-            if nargin <5
-                      gweights=sumcombineind(double(weights),double(list),double(obj.indsortlist));
-            end
-            mode=0;
-            switch combinemode
-                case 'sum'
-                    v2=v;  
-                case 'mean'
-                    v2=v.*weights;                 
-                case 'square'
-                    v2=v.^2.*weights.^2;
-                    gweights=sumcombineind(double(weights.^2),double(list),double(obj.indsortlist));
-%                     weights=weights.^2;
-                case 'min'
-                    v2=-v;
-                    mode=1;
-                case 'max'
-                    v2=v;
-                    mode=1;
-                case 'first'
-                    mode=2;
-                    v2=v;   
-                case 'locp'
-                    v2=1./v.^2;  
-            end
-            
-            
-
-            %combine modes
-            %<x>
-            %sqrt(<x^2>)
-            %first
-            %sum
-%             [listsort,indsort]=sort(list);
-            indsort=obj.indsortlist;
-%             listsort=list(indsort);
-%             vwsort=v2(indsort);
-
-            if mode==2
-%                 listsort=list(indsort);
-                vwsort=v2(indsort);
-                
-%                 dl=[diff(listsort);1];
-                vwout2=vwsort(obj.dllistsort>0);
-%                 listsort(end)
-%                 sum(dl==1)
-            else 
-                if mode==0
-                    vwout=sumcombineind(double(v2),double(list),(indsort));
-%                 vwout=sumcombine(double(vwsort),double(listsort));
-%                 gweights=sumcombine(double(weights(indsort)),double(listsort));
-                switch combinemode
-                    case 'sum'
-                        vwout2=vwout;  
-                    case 'mean'
-%                         gweights=sumcombineind(double(weights),double(list),(indsort));
-                        vwout2=vwout./gweights;                 
-                    case 'square'
-%                         gweights=sumcombineind(double(weights),double(list),(indsort));
-                        vwout2=sqrt(vwout./gweights); 
-                    case 'locp'
-                        vwout2=1./sqrt(vwout);
-                end   
-                elseif mode==1
-                    vwout=maxcombine(double(v2),double(list),(indsort));
-                    if strcmp(combinemode,'min')
-                        vwout2=-vwout;
+            else %group only single field            
+                if nargin>2
+                    obj.combinemodes.(field)=combinemode;
+                elseif  nargin==2
+                    if isfield(obj.combinemodes,field)
+                        combinemode=obj.combinemodes.(field);
+                    elseif length(field)>=3 && strcmp(field(end-2:end),'err')
+                        combinemode='locp';
                     else
-                        vwout2=vwout;
+                        combinemode='mean';
                     end
                 end
+
+                vtype=obj.locData.getloc(field).(field)(1);
+                if iscell(vtype)
+                    return
+                end
+                v=double(obj.locData.getloc(field).(field));
+
+                list=obj.locData.getloc('groupindex').groupindex;
+
+                if nargin <4
+                    weights=ones(size(v),'like',v);              
+                end   
+                if nargin <5
+                          gweights=sumcombineind(double(weights),double(list),double(obj.indsortlist));
+                end
+
+                cmode=0;
+                switch combinemode
+                    case 'sum'
+                        v2=v;  
+                    case 'mean'
+                        v2=v.*weights;                 
+                    case 'square' %currently not used
+                        v2=v.^2.*weights.^2;  %really weights^2?
+                        if nargin <6 
+                            gweights2=sumcombineind(double(weights.^2),double(list),double(obj.indsortlist));
+                        end
+                    case 'min'
+                        v2=-v;
+                        cmode=1;
+                    case 'max'
+                        v2=v;
+                        cmode=1;
+                    case 'first'
+                        cmode=2;
+                        v2=v;   
+                    case 'locp'
+                        v2=1./v.^2;  
+                end
+
+            %combine modes: <x>, sqrt(<x^2>), first, sum
+                indsort=obj.indsortlist;
+                if cmode==2 %first
+                    vwsort=v2(indsort);
+                    vwout2=vwsort(obj.dllistsort>0);
+                else 
+                    if cmode==0 %sum up: for sum and average
+                        vwout=sumcombineind(double(v2),double(list),(indsort));
+                    switch combinemode
+                        case 'sum'
+                            vwout2=vwout;  
+                        case 'mean'
+                            vwout2=vwout./gweights;                 
+                        case 'square'
+                            vwout2=sqrt(vwout./gweights2); 
+                        case 'locp'
+                            vwout2=1./sqrt(vwout);
+                    end   
+                    elseif cmode==1 %min/max
+                        vwout=maxcombine(double(v2),double(list),(indsort));
+                        if strcmp(combinemode,'min')
+                            vwout2=-vwout;
+                        else
+                            vwout2=vwout;
+                        end
+                    end
+                end
+                obj.locData.grouploc.(field)=cast(vwout2,'like',vtype);              
             end
-           
-            obj.locData.grouploc.(field)=cast(vwout2,'like',vtype);   
-            
-            end
-%             obj.status('group: combine fields done')
         end
     end
 end
