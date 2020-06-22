d8570c8d8dd925be83af3ce9f7c667378461b580
ming808 <liyiming12@gmail.com>
2020-06-22 14:38:09 +0200

    KinetochorePro: display unused clusters and cells.

--- plugins/+ROIManager/+Analyze/scripts/kinetochore/Kinetochore_distancesCC.m
+++ plugins/+ROIManager/+Analyze/scripts/kinetochore/Kinetochore_distancesCC.m
@@ -2,35 +2,20 @@ sites=g.locData.SE.sites;
 cells=getFieldAsVector(sites,'info.cell');
 cellnumbers=unique(cells);
 
-
-imout=0*sites(1).evaluation.shiftCrossCorrelation.xcorr;
-dx1=zeros(length(cellnumbers),1);
-dy1=zeros(length(cellnumbers),1);
-dx2=zeros(length(cellnumbers),1);
-dy2=zeros(length(cellnumbers),1);
-
 lUsed = getFieldAsVector(sites, 'annotation.use');
-indCells_unused = unique(getFieldAsVector(sites(~lUsed), 'info.cell'));
 
-cellnumbers = cellnumbers(cellnumbers~=indCells_unused);
-
-for k=length(cellnumbers):-1:1
-    indcells=find(cells==cellnumbers(k));
-    indcellAll(2*k-1:2*k) = indcells;
-    imout=sites(indcells(1)).evaluation.shiftCrossCorrelation.xcorr+imout;
-    imout=sites(indcells(2)).evaluation.shiftCrossCorrelation.xcorr+imout;
-    dx1(k)=sites(indcells(1)).evaluation.shiftCrossCorrelation.dxline;
-    dy1(k)=sites(indcells(1)).evaluation.shiftCrossCorrelation.dyline;
-    dx2(k)=sites(indcells(2)).evaluation.shiftCrossCorrelation.dxline;
-    dy2(k)=sites(indcells(2)).evaluation.shiftCrossCorrelation.dyline;
-end
-dx=zeros(length(indcellAll),1);
-dy=zeros(length(indcellAll),1);
-for k=indcellAll
-    dx(k)=sites(k).evaluation.shiftCrossCorrelation.dxline;
-    dy(k)=sites(k).evaluation.shiftCrossCorrelation.dyline;
+cellnumbers_un = [];
+%% separate the used and unused sites
+if length(sites(~lUsed))>0
+    indCells_unused = unique(getFieldAsVector(sites(~lUsed), 'info.cell'));
+    cellnumbers_un = [cellnumbers_un cellnumbers(indCells_unused)];
+    cellnumbers(indCells_unused) = [];
+    %cellnumbers = cellnumbers(cellnumbers~=indCells_unused);
+    [imout_un, dx1_un, dy1_un, dx2_un, dy2_un, dx_un, dy_un, indcellAll_un] = extractStat(cellnumbers_un, sites, cells);
 end
 
+%% extract statistics
+[imout, dx1, dy1, dx2, dy2, dx, dy, indcellAll] = extractStat(cellnumbers, sites, cells);
 
 maxprecision=0.1;
 pixrec=sites(1).evaluation.shiftCrossCorrelation.GuiParameters.pixrec;
@@ -41,6 +26,9 @@ pixrec=sites(1).evaluation.shiftCrossCorrelation.GuiParameters.pixrec;
 dxline=(xm-size(imouthr,1)/2)*maxprecision;
 dyline=(ym-size(imouthr,2)/2)*maxprecision;
 
+%% make plots
+
+% Average CC
 pr='%2.1f';
 
 dn=(size(imout,1)-1)/2;
@@ -54,8 +42,18 @@ plot(dyline,dxline,'+')
 title([dxline, dyline])
 subplot(2,2,2);
 
-ID = getFieldAsVector(g.locData.SE.sites,'ID');
-plotSElink(dx,dy,ID,g.locData.SE,'o')
+% Per cluster statistics
+ID = getFieldAsVector(g.locData.SE.sites(indcellAll),'ID');
+ID_un = getFieldAsVector(g.locData.SE.sites(indcellAll_un),'ID');
+
+plotSElink(dx,dy,ID,g.locData.SE,'ok')
+
+if length(sites(~lUsed))>0
+    hold on
+    plotSElink(dx_un,dy_un,ID_un,g.locData.SE,'xk')
+    hold off
+end
+
 [stxtx, smx, ssx]=mean2str([dx1;dx2],pr);
 [stxty, smy, ssy]=mean2str([dy1;dy2],pr);
 title({stxtx; stxty})
@@ -63,11 +61,17 @@ title({stxtx; stxty})
 subplot(2,2,3)
 hold off
 
-plot([dx1,dx2]',[dy1,dy2]')
+if length(sites(~lUsed))>0
+    plot([[dx1; dx1_un],[dx2; dx2_un]]',[[dy1; dy1_un],[dy2; dy2_un]]')
+else
+    plot([dx1 dx2]',[dy1 dy2]')
+end
 hold on
 
 plot((dx1+dx2)/2,(dy1+dy2)/2,'ko')
-
+if length(sites(~lUsed))>0
+    plot((dx1_un+dx2_un)/2,(dy1_un+dy2_un)/2,'kx')
+end
 plot(0,0,'k*')
 [ctxtx, cmx, csx]=mean2str((dx1+dx2)/2,pr);
 [ctxty, cmy, csy]=mean2str((dy1+dy2)/2,pr);
@@ -89,4 +93,30 @@ function [out,ms,ss]=mean2str(x,pr)
 [m,s]=robustMean(x);
 out=[num2str(m,pr) '\pm' num2str(s,pr)];
 ms=num2str(m);ss=num2str(s);
+end
+
+function [imout, dx1, dy1, dx2, dy2, dx, dy, indcellAll] = extractStat(cellnumbers, sites, cells)
+    imout=0*sites(1).evaluation.shiftCrossCorrelation.xcorr;
+    dx1=zeros(length(cellnumbers),1);
+    dy1=zeros(length(cellnumbers),1);
+    dx2=zeros(length(cellnumbers),1);
+    dy2=zeros(length(cellnumbers),1);
+    for k=length(cellnumbers):-1:1
+        indcells=find(cells==cellnumbers(k));
+        indcellAll(2*k-1:2*k) = indcells;
+        imout=sites(indcells(1)).evaluation.shiftCrossCorrelation.xcorr+imout;
+        imout=sites(indcells(2)).evaluation.shiftCrossCorrelation.xcorr+imout;
+        % dx and dy for the both clusters in one cell
+        dx1(k)=sites(indcells(1)).evaluation.shiftCrossCorrelation.dxline;
+        dy1(k)=sites(indcells(1)).evaluation.shiftCrossCorrelation.dyline;
+        dx2(k)=sites(indcells(2)).evaluation.shiftCrossCorrelation.dxline;
+        dy2(k)=sites(indcells(2)).evaluation.shiftCrossCorrelation.dyline;
+    end
+    dx=zeros(length(indcellAll),1);
+    dy=zeros(length(indcellAll),1);
+
+    for k=1:length(indcellAll)
+        dx(k)=sites(indcellAll(k)).evaluation.shiftCrossCorrelation.dxline;
+        dy(k)=sites(indcellAll(k)).evaluation.shiftCrossCorrelation.dyline;
+    end
 end
