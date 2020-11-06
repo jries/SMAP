0bcd952b06e0cdce4f0612d9aadee71b3f81317f
Rieslab <jonas.ries@gmail.com>
2020-11-06 13:54:42 +0100

    additions

--- plugins/+ROIManager/+Analyze/scripts/BeadBleachingBlinking.m
+++ plugins/+ROIManager/+Analyze/scripts/BeadBleachingBlinking.m
@@ -1,5 +1,8 @@
+if exist('obj','var')
+    sites=obj.locData.SE.sites;
+else
 sites=g.locData.SE.sites;
-
+end
 
 
 figure(239);
@@ -9,9 +12,9 @@ frames=(1:1000)';
 photall=0*frames;
 ft = fittype('a*0+b*exp(-x*c)');
 for k=length(sites):-1:1
-    frame=double(sites(k).evaluation.beadanalysis_2.frame);
+    frame=double(sites(k).evaluation.beadanalysis.frame);
     [frame,ia]=unique(frame);
-    phot=double(sites(k).evaluation.beadanalysis_2.phot(ia));
+    phot=double(sites(k).evaluation.beadanalysis.phot(ia));
     phot=phot-mean(phot(end-30:end));
     plot(frame,phot)
     hold on
@@ -38,22 +41,23 @@ plot(frames,fita(frames));
 ylabel('photons')
 xlabel('frames')
 out.decayall=fita.c;
-title(['t1/2=' num2str(1/fita.c)])
+title(['t1/2=' num2str(1/fita.c,'%5.0f')])
 
 
-noise=getFieldAsVector(sites,'evaluation.beadanalysis_2.noise');
+noise=getFieldAsVector(sites,'evaluation.beadanalysis.noise');
 subplot(2,4,4)
-photrange=0:1000:max(phot);
-histogram(phot,photrange);
+photrange=0:1000:max(phot0)*1.1;
+histogram(phot0,photrange);
 % plot(phot0,noise,'+')
-title('phot (t=0)');
+title(['phot (t=0), mean: ' num2str(mean(phot0),'%5.0f')]);
 % ylabel('noise')
 % 
 subplot(2,4,7)
 histogram(noise,10)
-title('noise')
+title(['noise mean: ' num2str(mean(noise),'%5.0f')])
+% title('noise')
 % plot(phot0,decay,'+')
 % xlabel('phot (t=0)');
 subplot(2,4,8)
 histogram(decay,10)
-title('decay')
+title(['decay, mean: ' num2str(mean(decay),'%5.0f')])
