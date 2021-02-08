e7a8ceb9892c2344f927034f0d7a1b933851eae3
Jonas Ries <jonas.ries@gmail.com>
2021-02-08 18:21:49 +0100

    save mp4 implemented

--- shared/myfunctions/mysavemovie.m
+++ shared/myfunctions/mysavemovie.m
@@ -1,10 +1,25 @@
 function mysavemovie(img,fsave,varargin)
 % varargin: name, value pairs to be passed on to movie constructor
+% look at documentation from VideoWriter
+% 'profile': standard VideoWriter profile. Default: MPEG-4
 % img: frames or array
 % fsave: output path
 
-v=VideoWriter([pf fout],'MPEG-4');
-v.FrameRate=obj.getSingleGuiParameter('framerate');
+% framerate
+ind=find(strcmp(varargin,'profile'),1,'first');
+if ~isempty(ind)
+    profile=varargin{ind+1};
+    varargin(ind:ind+1)=[];
+else
+    profile='MPEG-4';
+end
+
+v=VideoWriter(fsave,profile);
+
+for k=1:2:length(varargin)
+    set(v,varargin{k},varargin{k+1});
+end
+
 open(v);
-writeVideo(v,obj.outmovie(1:end-1))
+writeVideo(v,img)
 close(v);
