a20e28130da0e25e069b17896895fb6a2ed8c421
Jonas Ries <jonas.ries@gmail.com>
2021-03-24 17:30:32 +0100

    extend minflux loader

--- plugins/+File/+Load/Loader_minflux_json.m
+++ plugins/+File/+Load/Loader_minflux_json.m
@@ -43,7 +43,7 @@ function loadfile(obj,p,file)
 switch ext
     case '.mat'
         jt=load(file);
-        loc=minfluxmat2loc(jt);
+        loc=minfluxmat2loc(jt,p.onlyvalid,~p.simple);
     case '.json'
         txt=fileread(file);
         jt=jsondecode(txt);
@@ -136,25 +136,60 @@ loc.xnm=loc.xnm-min(loc.xnm);
 loc.ynm=loc.ynm-min(loc.ynm);
 end
 
-function loc=minfluxmat2loc(jt)
-valid=[jt.vld];
-% jtv=jt(valid);
-% for k=length(jtv):-1:1
-    locs=single(jt.loc(valid,:)*1e9);
-    loc.xnm(:,1)=locs(:,1);
-    loc.ynm(:,1)=locs(:,2);
-    if size(locs,3)>2
-        loc.znm(:,1)=locs(:,3);
-    end
-    loc.time(:,1)=single(jt.tim(valid))*1e3;  %from seconds to milliseconds
-    loc.iter(:,1)=single(jt.itr(valid));
-    loc.cfr(:,1)=single(jt.cfr(valid));
-    loc.dcr(:,1)=single(jt.dcr(valid));
-    loc.frame(:,1)=1:length(loc.xnm);
-% end
+function loc=minfluxmat2loc(jt, onlyvalid,loadall)
+numiter=size(jt.cfr,2);
+locs=single(jt.loc*1e9);
+if onlyvalid
+    valid=find(jt.vld);
+    indx=sub2ind(size(locs),valid,numiter*ones(size(valid)),ones(size(valid)));
+    indy=sub2ind(size(locs),valid,numiter*ones(size(valid)),2*ones(size(valid)));
+    indz=sub2ind(size(locs),valid,numiter*ones(size(valid)),3*ones(size(valid)));
+    ind2=sub2ind(size(jt.cfr),valid,numiter*ones(size(valid)));
+    ind1=jt.vld; 
+else  
+    lx=jt.loc(:,:,1);
+    lxg=~isnan(lx);
+    cols=1:numiter;
+    lxgc=cols.*lxg;
+    lm=max(lxgc,[],2);
+    goodind=lm>0;
+    g1=find(goodind);g2=lm(goodind);
+    indx=sub2ind(size(locs),g1,g2,ones(size(g1)));
+    indy=sub2ind(size(locs),g1,g2,2*ones(size(g1)));
+    indz=sub2ind(size(locs),g1,g2,3*ones(size(g1)));
+    ind2=sub2ind(size(jt.cfr),g1,g2);
+    ind1=goodind;
+    loc.vld(:,1)=jt.vld(ind1);
+    loc.iterations(:,1)=g2;
+end
 
-loc.xnm=loc.xnm-min(loc.xnm);
-loc.ynm=loc.ynm-min(loc.ynm);
+loc.xnm(:,1)=locs(indx);
+loc.ynm(:,1)=locs(indy);
+znm=locs(indz);
+if any(znm>0)
+    loc.znm(:,1)=locs(indz);
+end
+loc.time(:,1)=single(jt.tim(ind1))*1e3;  %from seconds to milliseconds
+loc.frame(:,1)=1:length(loc.xnm);
+loc.dcr(:,1)=single(jt.eco(ind2));
+loc.cfr(:,1)=single(jt.cfr(ind2));
+
+if loadall
+    loc.eco(:,1)=single(jt.eco(ind2));
+    loc.ecc(:,1)=single(jt.ecc(ind2));
+    loc.efo(:,1)=single(jt.efo(ind2));
+    loc.efc(:,1)=single(jt.efc(ind2));
+    loc.sta(:,1)=single(jt.sta(ind2));
+    if isfield(jt,'fbg')
+        loc.fbg(:,1)=single(jt.fbg(ind2));
+    end
+    loc.tid(:,1)=single(jt.tid(ind1));
+    loc.act(:,1)=single(jt.act(ind1));
+    loc.sky(:,1)=single(jt.sky(ind1));
+end
+
+loc.xnm=loc.xnm-min(loc.xnm(loc.vld));
+loc.ynm=loc.ynm-min(loc.ynm(loc.vld));
 end
 
 function pard=guidef(obj)
@@ -165,5 +200,14 @@ pard.plugininfo=info;
 pard.plugininfo.type='LoaderPlugin';
 pard.plugininfo.description='loades localzation data from a variety of files including text (.csv, .txt), hdf5 or MATLAB files. Localization data properties can be converted to those used in SMAP, and conversions can be saved for repeated use.';
 
+pard.simple.object=struct('Style','checkbox','String','load only main fields','Value',1);
+pard.simple.position=[1,1];
+pard.simple.Width=2;
+pard.simple.TooltipString='Only load main localization attributes needed for rendering.';
+
+pard.onlyvalid.object=struct('Style','checkbox','String','load only valid','Value',1);
+pard.onlyvalid.position=[2,1];
+pard.onlyvalid.Width=2;
+pard.onlyvalid.TooltipString='Load only localizations with the value tag == true.';
 
 end
