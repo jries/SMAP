34a2a8a72470011d201deba36769695772bd2332
Yu-Le Wu <yu-le.wu@embl.de>
2021-05-28 11:29:01 +0200

    added guiStyle.m

---
+++ shared/myfunctions/guiStyle.m
@@ -0,0 +1,64 @@
+function guiStyle(guihandles, fn, varargin)
+    p = inputParser;
+    p.addParameter('exclude',[])
+    p.addParameter('mode','default')
+    p.addParameter('fontsize',10)
+    p.addParameter('FieldHeight',26)
+    p.addParameter('tabsize1',[0    -1  546 342])
+    p.addParameter('tabsize2',[0    -1  540 311])
+    p.addParameter('Vsep',3)
+    p.addParameter('Xrim',3)
+    p.addParameter('Vrim',2)
+    p.parse(varargin{:});
+    p = p.Results;
+    switch p.mode
+        case 'default'
+            if ispc
+                fontsize=9;
+                FieldHeight=26;
+                Vsep=3;
+                Xrim=7;
+                Vrim=15;
+            elseif ismac
+                fontsize=15;
+                FieldHeight=25;
+                Vsep=1;
+                Xrim=2;
+                Vrim=0;
+            elseif isunix
+                fontsize=9;
+                FieldHeight=26;
+                Vsep=3;
+                Xrim=7;
+                Vrim=2;
+            end
+        case 'userDefined'
+            fontsize=9;
+            FieldHeight=26;
+            Vsep=3;
+            Xrim=7;
+            Vrim=2;
+    end
+	FieldWidth = FieldHeight*4;
+    Xsep = 1;
+    
+    lKept = ~strcmp(p.exclude, fn);
+    fn = fn(lKept);
+    for k = 1:length(fn)
+        h = guihandles.(fn{k});
+        originalUnits_hP = h.Parent.Units;
+        h.Parent.Units = 'pixels';
+        
+        edgeLen_hP = h.Parent.Position(3:4);
+        pos = h.Position;
+        
+        pos(1) = Xrim + (pos(1)-1)*(Xsep + FieldWidth);
+        pos(2) = edgeLen_hP(2) - (Vrim + pos(2)*(Vsep+FieldHeight));
+        pos(3:4) = [pos(3)*FieldWidth pos(4)*FieldHeight];
+        
+        h.Units = 'pixels';
+        h.FontSize = fontsize;
+        h.Position = pos;
+        h.Parent.Units = originalUnits_hP;
+    end
+end
