d1ff17166197f0fff22f2126afe913fb6c316335
Yu-Le Wu <yu-le.wu@embl.de>
2021-08-11 11:35:41 +0200

    LocMoFit_docs: layout changes

--- LocMoFit/docs/conf.py
+++ LocMoFit/docs/conf.py
@@ -41,6 +41,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Define the the document that contains the root toctree directive
+root_doc = 'contents.rst';
 
 # -- Options for HTML output -------------------------------------------------
 
