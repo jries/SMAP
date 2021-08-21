a8871c36e125004a4674b728eaa39ae7cde1263a
ming808 <liyiming12@gmail.com>
2021-08-21 17:45:04 +0800

    multi channel Gauss example code

--- fit3Dcspline/GlobLoc/Example_GlobalFit_Gauss.m
+++ fit3Dcspline/GlobLoc/Example_GlobalFit_Gauss.m
@@ -1,19 +1,16 @@
-%% test Gauss multi channel fit
+%% Gauss
 
-
-Nfits = 1000;
-Nphotons1 =2500;
+Nfits=1000    %number of images to fit
+bg=20;           %background fluorescence in photons/pixel/frame
+Nphotons1=2500;   %expected photons/frame
 Nphotons2 =2500;
-Npixels = 15;
 bg1 = 20;
-bg2 = 20;
-zoffset = 0;%nm
+bg2 = 20
+% Npixels=9;      %linear size of fit region in pixels. 
+PSFsigma1=1.2; 
+PSFsigma2=1.5;%PSF sigma in pixels
+fittype=1;%1 for Gauss fit, 2 for spline fit
 
-% ztruthAll = -600:50:600;
-% numz=length(ztruthAll);
-
-% zstartnm = 200;
-% zstart=repmat([-2 -1 1 2]*zstartnm/dz1,[Nfits 1]);
 theta = 1*pi/180;%rotation
 sx = 1.01;%scalex
 sy = 1.02;%scaley
@@ -27,122 +24,54 @@ tformT = [1 0 tx; 0 1 ty; 0 0 1];
 tformF = tformR*tformS*tformT;
 tforminv=inv(tformF);
 
-PSFsigma1=1.2;
-PSFsigma2=2;%PSF sigma in pixels
-fittype=1;
-
-
-coordsxy1 = Npixels/2 -1 +2*rand([Nfits 2]);
-coordsxy2 = zeros(Nfits,2);
-
-for i = 1:Nfits
-    temp = tformF*[coordsxy1(i,:)';1];
-    coordsxy2(i,:) = temp(1:2);
-end
-
-
-%   Generate a stack of images
-% coords=Npixels/2-1+rand([Nfits 2])+[0*ones(Nfits,1) zeros(Nfits,1)];
-[output1] = finitegausspsf(Npixels,PSFsigma1,Nphotons1,bg1,coordsxy1);
-[output2] = finitegausspsf(Npixels,PSFsigma2,Nphotons2,bg2,coordsxy2);
-output1 = poissrnd(output1,Npixels,Npixels,Nfits);
-output2 = poissrnd(output2,Npixels,Npixels,Nfits);
-d_data(:,:,:,1) = output1;
-d_data(:,:,:,2) = output2;
-
-noChannels = 2; % # of channels
-dT = zeros(5,noChannels,Nfits);
-dxy=coordsxy1-coordsxy2;
-temp = reshape(dxy',[2 1,Nfits]);
-dT(1:2,2,:)=temp*-1;
-dT(5,2,:)=PSFsigma2-PSFsigma1; % parameter shifts between channels
-
-% define scaling between channels
-dS = repmat([1, 1 ;1, 1 ;1, 1;1, 1;1, 1],[1 1 Nfits]);
-
-% transformation between channels
-dTS = zeros(5,noChannels*2,Nfits);
-dTS(:,1:2,:)=dT;
-dTS(:,3:4,:)=dS;
-iterations = 50;
+noChannels = 2; %number of channels
+iterations=50;  %iteration time
 sCMOSvarmap = 0;
 silent = 0;
-
-shared_linkALL = repmat([1;1;1;1;1], [1 Nfits]); % link all parameters, XYZ,Photons and background
-
-
-
-
- [P_linkALL,CRLB_linkALL, LL1_linkALL] =  mleFit_LM_globalfit(d_data,fittype,shared_linkALL,iterations,single(1),dTS,sCMOSvarmap,silent);
-
-
-
-
-
-
-
-%% Gauss
-
-Nfits=500    %number of images to fit
-bg=10;           %background fluorescence in photons/pixel/frame
-Nphotons=2500;   %expected photons/frame
-% Npixels=9;      %linear size of fit region in pixels. 
-PSFsigma1=1.2; 
-PSFsigma2=1.5;%PSF sigma in pixels
-fittype=2;
-
+initSigma = 1.5
 n=1;
-for Npixels = 7:19
+for Npixels = 11:15
     %   Generate a stack of images
-    coords=Npixels/2-1+rand([Nfits 2])+[0*ones(Nfits,1) zeros(Nfits,1)];
-    [out] = finitegausspsf(Npixels,PSFsigma1,Nphotons,bg,coords);
-    
-    %   Corrupt with Poisson noise
-    data = poissrnd(out,Npixels,Npixels,Nfits);
-    %data = poissrnd(out); %requires statistics toolbox
-    %   Can look at data (DipImage)
-    %dipshow(permute(data,[2 1 3])); %dipimage permuted 1st two dimensions
-    
-    coords1 = coords;
-    data1 = data;
-    
-    % coords=Npixels/2-1+rand([Nfits 2])+[0*ones(Nfits,1) zeros(Nfits,1)];
-    coords = coords1+1;
-    [out] = finitegausspsf(Npixels,PSFsigma2,Nphotons,bg,coords);
-    
-    %   Corrupt with Poisson noise
-%     data=single(noise(out,'poisson',1)); %requires DipImage
-    data = poissrnd(out,Npixels,Npixels,Nfits);
-% output2 = poissrnd(output2,Npixels,Npixels,Nfits);
-    coords2 = coords;
-    data2 = data;
+    coordsxy1 = Npixels/2 -1 +2*rand([Nfits 2]);
+    coordsxy2 = zeros(Nfits,2);
+    for i = 1:Nfits
+        temp = tformF*[coordsxy1(i,:)';1];
+        coordsxy2(i,:) = temp(1:2);
+    end
     
     
-    d_data(:,:,:,1) = data1;
-    d_data(:,:,:,2) = data2;
+    [output1] = finitegausspsf(Npixels,PSFsigma1,Nphotons1,bg1,coordsxy1);
+    [output2] = finitegausspsf(Npixels,PSFsigma2,Nphotons2,bg2,coordsxy2);
+    output1 = poissrnd(output1,Npixels,Npixels,Nfits);
+    output2 = poissrnd(output2,Npixels,Npixels,Nfits);
+    d_data(:,:,:,1) = output1;
+    d_data(:,:,:,2) = output2;
     
     
-    noChannels = 2;
-    iterations=50;
+    
     dT = zeros(5,noChannels,Nfits);
-    dxy=coords1-coords2;
+    dxy=coordsxy1-coordsxy2;
     temp = reshape(dxy',[2 1,Nfits]);
-    dT(1:2,2,:)=temp*(-1);
-    % dT(5,2,:)=0.8;
+    dT(1:2,2,:)=temp*-1;
+    dT(5,2,:)=PSFsigma2-PSFsigma1; % parameter shifts between channels
     
-    shared = [1;1;1;1;0];
-    sharedA = uint32(repmat(shared,[1 Nfits]));
-    fittype = uint32(2);
-    initSigma = single(1.2);
-    dT = single(dT);
-    d_data(:,:,:,1) = data1;
-    d_data(:,:,:,2) = data2;
+    % define scaling between channels
+    dS = repmat([1, 1 ;1, 1 ;1, 1;1, 1;1, 1],[1 1 Nfits]);
+    
+    % transformation between channels
+    dTS = zeros(5,noChannels*2,Nfits);
+    dTS(:,1:2,:)=dT;
+    dTS(:,3:4,:)=dS;
+
+    
+    shared = repmat([1;1;1;1;0], [1 Nfits]); % link all parameters, XYZ,Photons and background
+
+    
+   
     
 
     tic
-%     [PG,CRLBG, LLG] =  GPUmleFit_LM_MultiChannel(d_data,fittype,(sharedA),50,initSigma,(dT));
-    [PGC,CRLBGC, LLGC] =  CPUmleFit_LM_MultiChannel(single(d_data),fittype,(sharedA),50,initSigma,(dT));
-
+     [P,CRLB, LL1] =  mleFit_LM_globalfit(d_data,fittype,shared,iterations,initSigma,dTS,sCMOSvarmap,silent);
     tGauss=toc;
     
     GPUmleFit_LM_MultiChannel_Gauss(n,1)=Nfits/tGauss;
