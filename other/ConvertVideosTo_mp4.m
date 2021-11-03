41f0d9f32259f294bb6912f42c15bed823eedcb3
Jonas Ries <jonas.ries@gmail.com>
2021-11-03 14:43:21 +0100

    extended to tif

--- other/ConvertVideosTo_mp4.m
+++ other/ConvertVideosTo_mp4.m
@@ -1,9 +1,22 @@
 global pn
 
-[fn,pn]=uigetfile([pn filesep '*.avi']);
+[fn,pn]=uigetfile([pn filesep '*.*']);
 
-v=VideoReader([pn filesep fn]);
+[~,~,ext]=fileparts(fn);
+switch ext
+    case '.tif'
 
-video=read(v);
-fout=[pn filesep strrep(fn,'.avi','_c.mp4')];
+        video=tiffreadVolume([pn filesep fn]);
+        video=permute(video,[1,2,4,3]);
+    otherwise
+        v=VideoReader([pn filesep fn]);
+        video=read(v);
+end
+% %convert 2 gray
+% saturation=1.5;
+% video=(sum(video,3));
+% video=uint8(video/max(video(:))*saturation*2^8);
+
+
+fout=[pn filesep strrep(fn,ext,'_c.mp4')];
 mysavemovie(video,fout,'FrameRate',v.FrameRate)
