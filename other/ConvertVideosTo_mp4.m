15446fe0312ad87ecee7ce4a149906606c37fad9
Jonas Ries <jonas.ries@gmail.com>
2021-11-14 10:09:00 +0100

    bugfix load many layers

--- other/ConvertVideosTo_mp4.m
+++ other/ConvertVideosTo_mp4.m
@@ -1,22 +1,29 @@
 global pn
+framerate=15;
 
 [fn,pn]=uigetfile([pn filesep '*.*']);
 
 [~,~,ext]=fileparts(fn);
 switch ext
     case '.tif'
-
         video=tiffreadVolume([pn filesep fn]);
         video=permute(video,[1,2,4,3]);
+    case '.gif'
+        [img,map]=imread([pf f],'Frames','all');
+        img=squeeze(img)*intensityscaling;
+        clear video;
+        for k=1:size(img,3)
+            video(k)=im2frame(img(:,:,k),map);
+        end
     otherwise
         v=VideoReader([pn filesep fn]);
         video=read(v);
+        framerate=v.FrameRate;
 end
 % %convert 2 gray
 % saturation=1.5;
 % video=(sum(video,3));
 % video=uint8(video/max(video(:))*saturation*2^8);
 
-
 fout=[pn filesep strrep(fn,ext,'_c.mp4')];
-mysavemovie(video,fout,'FrameRate',v.FrameRate)
+mysavemovie(video,fout,'FrameRate',framerate)
