ed466c87088693e74faa2436daca89b33596a08b
Jonas Ries <jonas.ries@gmail.com>
2021-11-23 13:20:07 +0100

    add graphics

--- plugins/+Analyze/+measure/Cluster_MINFLUX.m
+++ plugins/+Analyze/+measure/Cluster_MINFLUX.m
@@ -24,6 +24,39 @@ classdef Cluster_MINFLUX<interfaces.DialogProcessor
             sxdetrend=std(diff(locs.xnm))/sqrt(2);sydetrend=std(diff(locs.ynm))/sqrt(2);
             [~, sxrobust]=robustMean(locs.xnm); [~, syrobust]=robustMean(locs.ynm);
             
+            %graphs
+                 ff='%2.1f';
+            axx=obj.initaxis('x');
+            mx=mean(locs.xnm);
+            plot(axx,locs.time,locs.xnm-mx,locs.time,0*locs.time,'k', ...
+                locs.time,0*locs.time+sigmax,'c',locs.time,0*locs.time-sigmax,'r',...
+                locs.time,0*locs.time+sxrobust,'r',locs.time,0*locs.time-sxrobust,'c',...
+                locs.time,0*locs.time+sxdetrend,'m',locs.time,0*locs.time-sxdetrend,'m')
+            legend(axx,'data','','std','','robust std','','detrend std','')
+            xlabel(axx,'time (ms)')
+            ylabel(axx,'x (nm)')
+               title(axy,['std(x) = ' num2str(sigmax,ff) ' nm, std(x) robust = ' num2str(sxrobust,ff) ' nm, std(x) detrend = ' num2str(sxdetrend,ff) ' nm.'])
+
+
+            axy=obj.initaxis('y');
+            plot(axy,locs.time,locs.ynm-mean(locs.ynm),locs.time,0*locs.time,'k', ...
+                locs.time,0*locs.time+sigmay,'c',locs.time,0*locs.time-sigmay,'r',...
+                locs.time,0*locs.time+syrobust,'r',locs.time,0*locs.time-syrobust,'c',...
+                locs.time,0*locs.time+sydetrend,'m',locs.time,0*locs.time-sydetrend,'m')
+            legend(axy,'data','','std','','robust std','','detrend std','')
+            xlabel(axy,'time (ms)')
+            ylabel(axy,'y (nm)')
+            title(axy,['std(y) = ' num2str(sigmay,ff) ' nm, std(y) robust = ' num2str(syrobust,ff) ' nm, std(y) detrend = ' num2str(sydetrend,ff) ' nm.'])
+
+            axt=obj.initaxis('time');
+            histogram(axt,dt,dtmin/2:dtmin:quantile(dt,0.995))
+            hold(axt,'on')
+            histogram(axt,dt,0:dtmin*0.1:quantile(dt,0.995))
+            xlabel(axt,'dt (ms)')
+            ylabel(axt,'frequency')
+       
+            title(axt,['dtmin = ' num2str(dtmin,ff) ' ms, dtmedian = ' num2str(dtmedian,ff) ' ms, dtmean = ' num2str(dtmean,ff) ' ms.'])
+ 
             header=sprintf('filename \t dtmin \t dtmedian \t dtmean \t sigmax \t sigmay \t sigmaxrobust \t sigmayrobust \t sigmaxdetrend \t sigmaydetrend' );
             disp(header)
             results=sprintf([filename '\t' num2str(dtmin)  '\t' num2str(dtmedian) '\t' num2str(dtmean) '\t' num2str(sigmax) ...
