2cc54850e5409ab9d3de14a364e81e94e79d4c0b
Jonas Ries <jonas.ries@gmail.com>
2021-12-03 15:08:05 +0100

    own yaml writer

--- shared/myfunctions/WriteYamlSimple.m
+++ shared/myfunctions/WriteYamlSimple.m
@@ -1,5 +1,9 @@
-function WriteYamlSimple(sin, file)
-    txt=struct2yaml(sin,'')
+function txt=WriteYamlSimple(sin, file)
+    txt=struct2yaml(sin,'');
+    txto=txt(2:end);
+fid = fopen(file, 'wt');
+fprintf(fid, '%s\n', txto);
+fclose(fid);
 end
 
 function txt=struct2yaml(p,prefix)
@@ -11,39 +15,72 @@ if isstruct(p)||isobject(p)
         txt(end+1,1)=string([prefix fn{k} ':']);
         prefixn=[ prefix,'  '];
         to=struct2yaml(p.(fn{k}),prefixn);
-        for l=1:length(to)
+        txt(end,1)=txt(end,1)+" "+to(1);
+        for l=2:length(to)
             txt(end+1,1)=to(l);
         end
     end
 else
     if iscell(p)
-        to=p{1};
-        if isnumeric(to)||islogical(to)
-            to=num2str(to);
-        end
-        for k=2:length(p)
-            th=p{k};
-            if isnumeric(th)||islogical(th)
-                th=num2str(th);
-            end
-            if ischar(th)
-                to=[to ',' th];
-            end
-        end
-        p=to;
+        s=cells2string(p, [prefix(3:end) '  ']);
+        txt(end+1:end+length(s))=s;
+%         to=p{1};
+%         if isnumeric(to)||islogical(to)
+%             to=num2str(to);
+%         end
+%         for k=2:length(p)
+%             th=p{k};
+%             if isnumeric(th)||islogical(th)
+%                 th=num2str(th);
+%             end
+%             if ischar(th)
+%                 to=[to ',' th];
+%             end
+%         end
+%         p=to;
+    else
+        txt(1,1)=string(p);
     end
-    if isnumeric(p)||islogical(p)
-        p=num2str(p);
-    end
-    if ~isempty(p)
-        tx=p(1,:);
-        if ischar(tx)
-            txt=string([prefix ': ' tx]);
+%     if isnumeric(p)||islogical(p)
+%         p=num2str(p);
+%     end
+%     if ~isempty(p)
+%         tx=p(1,:);
+%         if ischar(tx)
+%             txt=string([prefix ': ' tx]);
+%         end
+%     else
+%         txt=string([prefix ': ']);
+%     end
+
+end
+%     txt=txt(2:end);
+end
+
+function s=cells2string(p, prefix)
+s="";
+for k=1:length(p)
+%     prefixh=prefix;
+%     if k>1
+%         prefixh(3)=' ';
+%     end
+prefix1=prefix;prefix1(end-1)='-';
+    if iscell(p{k})
+%         sh=cells2string(p{k},[prefixh '- ']);
+        sh=cells2string(p{k},'  ');
+        
+        s(end+1,1)=string(prefix1)+sh(1);
+        for ks=2:length(sh)
+            s(end+1,1)=string(prefix)+sh(ks);
         end
     else
-        txt=string([prefix ': ']);
+        ph=string(p{k});
+        if isempty(ph)
+            ph="null";
+        end
+        s(end+1,1)= string(prefix1)+ ph;
     end
 
 end
-    txt=txt(2:end);
+s=s(2:end,1);
 end
