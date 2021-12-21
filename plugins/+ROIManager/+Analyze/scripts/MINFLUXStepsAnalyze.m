a0980abfc9c2b61bf472027b2a7c89f3f1c3f6f2
Jonas Ries <jonas.ries@gmail.com>
2021-12-21 16:09:25 +0100

    plot steps

--- plugins/+ROIManager/+Analyze/scripts/MINFLUXStepsAnalyze.m
+++ plugins/+ROIManager/+Analyze/scripts/MINFLUXStepsAnalyze.m
@@ -4,11 +4,15 @@ sites=g.locData.SE.sites;
 stepsize=[]; %define the variables you want to read out
 steptime=[];
 
+pluginname='StepsMINFLUX';
+fieldname1='steps';
+
+
 for k=1:length(sites)
-    if ~isfield(sites(k).evaluation.StepsMINFLUX,'steps') % if no steps are found, look at next site
+    if ~isfield(sites(k).evaluation,pluginname) || ~isfield(sites(k).evaluation.(pluginname),fieldname1) % if no steps are found, look at next site
         continue
     end
-    sh=sites(k).evaluation.StepsMINFLUX.steps;
+    sh=sites(k).evaluation.(pluginname).(fieldname1);
     stepsize(end+1:end+length(sh.stepsize))=sh.stepsize; %add values from current site to the list
     steptime(end+1:end+length(sh.dwelltime))=sh.dwelltime;
 end
@@ -20,6 +24,8 @@ n=round(min(stepsize)):ds:max(stepsize);
 histogram(stepsize,n)
 xlabel('stepsize(nm)')
 
+dt=5;
+n=0:dt:max(steptime);
 subplot(2,2,2)
-histogram(steptime,20)
+histogram(steptime,n)
 xlabel('step time (ms)')
