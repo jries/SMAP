60eaae57d166924a782f0577d19cbadd1653b357
Jonas Ries <ries@embl.de>
2022-04-14 12:55:49 +0200

    image loader for Sheng's mat files implemented

--- shared/imageloaders/imageloader_mat.m
+++ shared/imageloaders/imageloader_mat.m
@@ -4,8 +4,11 @@ classdef imageloader_mat<interfaces.imageloaderSMAP
     properties
          reader
          batchlength
+         infofiles
+         infoall
          allfiles
          currentfiledata %.q1,..., .batch
+         lastimagepos
     end
     
     methods
@@ -17,12 +20,12 @@ classdef imageloader_mat<interfaces.imageloaderSMAP
             file=char(file);
             %extract pattern:
             ind=strfind(file,'_');
-            
             path=fileparts(file);
-            searchstr=[file(1:ind(end-1)) '*' file(ind(end):end)]
-            allfiles=dir(searchstr);
-            obj.allfiles.files={allfiles(:).name};
+            searchstr=[file(1:ind(end-1)) '*' file(ind(end):end)];
+            allf=dir(searchstr);
+            obj.allfiles.files={allf(:).name};
             obj.allfiles.path=path;
+            analyzecontent(obj);
             md=obj.getmetadata;
             getmetadatatagsi(obj);
             obj.metadata.basefile=path;
@@ -32,38 +35,30 @@ classdef imageloader_mat<interfaces.imageloaderSMAP
             obj.file=file;
         end
         function image=getimagei(obj,frame)
-%             try
-                batch=ceil(frame/obj.batchlength);
-                fnum=mod(frame-1,obj.batchlength)+1;
-                if batch~=obj.currentfiledata.batch
-                    if batch>length(obj.allfiles.files) 
-                        image=[];
-                        return;
-                    end
-                    obj.currentfiledata.frames=load([obj.allfiles.path filesep obj.allfiles.files{batch}]);
-                    obj.currentfiledata.batch=batch;
-                end
-                % XXXX how to deal with time vs z? we want to fit all.
-                % convert to a linear index?
-                if fnum<=size(obj.currentfiledata.frames.qd1,3)
-                    for k=4:-1:1 %maximally 4 channels
-                        if isfield(obj.currentfiledata.frames,[qd int2str(k)])
-                            image(:,:,1,k)=obj.currentfiledata.frames.([qd int2str(k)])(:,:,fnum);
-                        end
-                    end
-%                     image(:,:,1,1)=obj.currentfiledata.frames.qd1(:,:,fnum);
-%                     image(:,:,1,2)=obj.currentfiledata.frames.qd2(:,:,fnum);
-%                     image(:,:,1,3)=obj.currentfiledata.frames.qd3(:,:,fnum);
-%                     image(:,:,1,4)=obj.currentfiledata.frames.qd4(:,:,fnum);
-                else
+            batch=find(frame<=obj.infoall.framesinbatch,1,'last');
+            if batch>1
+                fnum=frame-obj.infoall.framesinbatch(batch-1);
+            else
+                fnum=frame;
+            end
+            if batch~=obj.currentfiledata.batch
+                if batch>length(obj.allfiles.files) 
                     image=[];
+                    return;
                 end
+                obj.currentfiledata.frames=load([obj.allfiles.path filesep obj.allfiles.files{batch}]);
+                obj.currentfiledata.batch=batch;
+            end
+            image=[];
+            [tind,zind]=ind2sub([obj.infoall.timepoints,obj.infoall.zslices],fnum);
+            if tind>obj.infoall.timepoints || zind>obj.infoall.zslices
+                return
+            end
 
-%                 image={obj.currentfiledata.frames.qd1(:,:,fnum),obj.currentfiledata.frames.qd2(:,:,fnum),obj.currentfiledata.frames.qd3(:,:,fnum),obj.currentfiledata.frames.qd4(:,:,fnum)};
-%             catch err
-%                 err
-%                 image=[];
-%             end
+            for k=obj.infoall.channels:-1:1
+                    image(:,:,1,k)=obj.currentfiledata.frames.(obj.infofiles(batch).name{k})(:,:,tind,zind);
+            end
+            obj.lastimagepos=[batch, tind, zind];
         end
         
         function closei(obj)
@@ -77,32 +72,62 @@ classdef imageloader_mat<interfaces.imageloaderSMAP
                     image=obj.getimage(number);
             end
         end
-        
+        function analyzecontent(obj)
+            numframes=0;
+            for k=1:length(obj.allfiles.files)
+                f1=[obj.allfiles.path filesep obj.allfiles.files{k}];
+                f1info=whos('-file',f1);
+                info(k).channels=0;
+                indch=1;
+                for l=1:length(f1info)
+                    if strcmpi(f1info(l).name,'metadata')
+                    elseif contains(f1info(l).name,'qd')
+                        sh=f1info(l).size;
+                        info(k).channels=info(k).channels+1;
+                        info(k).zslices(indch)=sh(4);
+                        info(k).timepoints(indch)=sh(3);
+                        info(k).name{indch}=f1info(l).name;
+                        indch=indch+1;
+                    else
+                        disp(f1info(l).name)
+                    end
+                end
+                numframes=numframes+max(info(k).zslices)*max(info(k).timepoints);
+                zslicesall(k)=max(info(k).zslices); timepointsall(k)=max(info(k).timepoints); 
+            end
+            obj.infoall.numframes=numframes;
+            obj.infoall.zslices=zslicesall;
+            obj.infoall.timepoints=timepointsall;
+            obj.infoall.framesinbatch=cumsum(zslicesall*timepointsall);
+            obj.infoall.channels=max([info(:).channels]);
+            obj.infofiles=info;
+            obj.infoall.imagesize=sh(1:2);
+        end
+
         function allmd=getmetadatatagsi(obj)
-            allmd={'Format','Mat'};
-                        % determine batch length
+            allmd={'Format','Mat'}; 
+            allmd(end+1,:)={'NumberOfChannels',obj.infoall.channels};
+            allmd(end+1,:)={'ZSlices',obj.infoall.zslices};
+            allmd(end+1,:)={'Timepoints',obj.infoall.timepoints};
+            allmd(end+1,:)={'Width info',obj.infoall.imagesize(2)};
+            allmd(end+1,:)={'Height info',obj.infoall.imagesize(1)};
             f1=[obj.allfiles.path filesep obj.allfiles.files{1}];
-            f1info=whos('-file',f1);
-            iqd1=find(contains({f1info(:).name},'qd1'));
-            allmd(end+1,:)={'batchlength',f1info(iqd1).size(3)};
-            allmd(end+1,:)={'NumberOfChannels',4};
-            obj.batchlength=f1info(iqd1).size(3);
-
-
-            allmd(end+1,:)={'Width info',f1info(iqd1).size(2)};
-            allmd(end+1,:)={'Height info',f1info(iqd1).size(1)};
-
             allmd(end+1,:)={'FileName',f1};
-            allmd(end+1,:)={'Roi direct',num2str([0 0 f1info(iqd1).size(2) f1info(iqd1).size(1)])};
-
-            fe=[obj.allfiles.path filesep obj.allfiles.files{end}];
-            feinfo=whos('-file',fe);
-            numf=(length(obj.allfiles.files)-1)*obj.batchlength+feinfo(iqd1).size(3);
-            allmd(end+1,:)={'Frames',numf};
-            allmd(end+1,:)={'frames direct',numf};
-            obj.allmetadatatags=allmd;
+            allmd(end+1,:)={'Roi direct',num2str([0 0 obj.infoall.imagesize(2) obj.infoall.imagesize(1)])};
+            allmd(end+1,:)={'Frames',obj.infoall.numframes};
+            allmd(end+1,:)={'frames direct',obj.infoall.numframes};
+            
 
             l=load(f1,'metadata');
+            list=mystruct2list(l.metadata);
+            allmd=vertcat(allmd,list);
+
+            obj.allmetadatatags=allmd;
+           
+
+            
+
         end       
     end    
-end
+    end
+
