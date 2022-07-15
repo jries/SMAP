48eb91e3b413923b37d27c62b666b1d01564f769
Yu-Le Wu <yu-le.wu@embl.de>
2022-07-15 10:50:47 +0200

    LocMoFit: documentation updates

--- LocMoFit/docs/conf.py
+++ LocMoFit/docs/conf.py
@@ -55,6 +55,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_theme_options = {
+    'prev_next_buttons_location': None
+}
+
 # -- matlab source code ------------------------------------------------------
 matlab_src_dir = os.path.abspath("..")
 
