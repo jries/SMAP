2671c7d1f8aa021289865becd2b242f7264b2eb2
Jonas Ries <ries@embl.de>
2023-05-08 18:41:39 +0200

    updat Jones

--- other/Jones_matrices.m
+++ other/Jones_matrices.m
@@ -1,29 +1,60 @@
 %Jones martrices
 v=[1;0];
 vstart=linear(pi/2);
+clear phases dp
+phases(1,1,:)=-pi/2:0.01:pi/2;
 
-phases=-pi/2:0.01:pi/2;
-% phases=[0 pi/4 pi];
-clear vphase
-for k=length(phases):-1:1
-  vphase(:,k)=phaseplate(phases(k))*vstart;
-end
-vrot=HWP45*vphase;
-vsum=(vrot+vphase);
-vout=polarizer(0)*vsum;
-outchannel=1;
-dangle=mod(angle(vrot(outchannel,:))-angle(vphase(outchannel,:))+pi,2*pi)-pi;
 
-figure(88);plot(phases,dangle,phases,abs(vout(outchannel,:)).^2/2,phases,0*phases)
+[vout, dangle]=EomSLM(vstart,phases,0);
+[vout1, dangle1]=EomSLM(vstart,phases,pi/2*1.9);
+[vout2, dangle2]=EomSLM(vstart,phases,-pi/2*1.9);
+sphases=squeeze(phases);
+
+figure(88);subplot(1,2,1);hold off;
+
+plot(sphases,squeeze(dangle),'r',sphases,squeeze(abs(vout(outchannel,:,:)).^2/2),'b',sphases,0*sphases,'k')
 legend('phase diff','intensity(0)')
 xlabel('phase shift')
+hold on
+plot(sphases,squeeze(dangle1),'r--',sphases,squeeze(abs(vout1(outchannel,:)).^2/2),'b--')
+plot(sphases,squeeze(dangle2),'r--',sphases,squeeze(abs(vout2(outchannel,:)).^2/2),'b--')
+title('EOM delay for 3 lateral phases')
 
+dp(1,1,:)=0:0.1:2*pi;
+[voutd, dangled]=EomSLM(vstart,-pi/4,dp);
+[voutd1, dangled1]=EomSLM(vstart,0,dp);
+[voutd2, dangled2]=EomSLM(vstart,pi/4,dp);
+sphases=squeeze(dp);
 
+figure(88);subplot(1,2,2);hold off;
+
+plot(sphases,squeeze(dangled),'r',sphases,squeeze(abs(voutd(outchannel,:,:)).^2/2),'b',sphases,0*sphases,'k')
+legend('phase diff','intensity(0)')
+xlabel('lateral position')
+hold on
+plot(sphases,squeeze(dangled1),'r--',sphases,squeeze(abs(voutd1(outchannel,:)).^2/2),'b--')
+plot(sphases,squeeze(dangled2),'r--',sphases,squeeze(abs(voutd2(outchannel,:)).^2/2),'b--')
+
+title('lateral phases for 3 EOM delays')
+
+function [vout, dangle]=EomSLM(vstart,phases,dphi)
+if nargin<3
+    dphi=0;
+end
+vphase=pagemtimes(phaseplate(phases),vstart);
+ % vphase=pagemtimes(QWP(phases),vstart);
+vphase=pagemtimes(addphase(dphi),vphase);
+vrot=pagemtimes(addphase(-dphi),pagemtimes(HWP45,vphase));
+vsum=(vrot+vphase);
+vout=pagemtimes(polarizer(0),vsum);
+outchannel=1;
+dangle=mod(angle(vrot(outchannel,:))-angle(vphase(outchannel,:))+pi,2*pi)-pi;
+end
 
 function out=waveplate(phase,angle)
-out=exp(-1i*phase/2)*...
-    [cos(angle)^2+exp(1i*phase)*sin(angle)^2 (1-exp(1i*phase))*cos(angle)*sin(angle)
-    (1-exp(1i*phase))* cos(angle)*sin(angle) sin(angle)^2+exp(1i*phase)*cos(angle)^2];
+out=exp(-1i*phase/2).*...
+    [cos(angle).^2+exp(1i*phase)*sin(angle).^2 (1-exp(1i*phase))*cos(angle).*sin(angle)
+    (1-exp(1i*phase))* cos(angle).*sin(angle) sin(angle).^2+exp(1i*phase)*cos(angle).^2];
 end
 
 function out=QWP(angle)
