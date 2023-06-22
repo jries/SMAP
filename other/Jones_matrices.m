6d809cbd4906d096154e461ec4d8547ca45a13d4
Jonas Ries <jonas.ries@gmail.com>
2023-06-22 17:45:01 +0200

    add Jones stuff

--- other/Jones_matrices.m
+++ other/Jones_matrices.m
@@ -1,64 +1,106 @@
 %Jones martrices
-v=[1;0];
-vstart=linear(pi/2);
+v=[0;1];
+ vlin45=linear(-pi/4);  
+
+
+vstart=v;
 clear phases dp
-phases(1,1,:)=-pi/2:0.01:pi/2;
+vstart=pagemtimes(QWP(pi/4),vstart); %QWP bias
 
 
-[vout, dangle]=EomSLM(vstart,phases,0);
-[vout1, dangle1]=EomSLM(vstart,phases,pi/2*1.9);
-[vout2, dangle2]=EomSLM(vstart,phases,-pi/2*1.9);
-sphases=squeeze(phases);
+
+dd=1/32;
+phases(1,1,:)=-1+dd:dd:1; 
+dphi=-1/4:1/4:1/2; %pi
+
 outchannel=1;
-figure(88);subplot(1,2,1);hold off;
+figure(88);
+clf
+subplot(1,2,1);hold off;
+sphases=squeeze(phases);
+col=lines(length(dphi));
+legends={};
+for k=1:length(dphi)
+    [vout, dangle]=EomSLM(vstart,phases*pi,dphi(k)*pi);
+    int0=squeeze(abs(vout(outchannel,:,:)).^2/2);
+    plot(sphases,int0,'Color',col(k,:))
+    [~,ind]=min(int0);
+    text(phases(ind),0.03,num2str(dphi(k)),"HorizontalAlignment","center","Color",'b')
+    hold on
+    legends{end+1}=['latpos = ' num2str(dphi(k)) ' pi'];
+end
+plot(sphases,0*sphases,'k')
+text(min(phases),0.03,'lateral pos',"HorizontalAlignment","right","Color",'b')
+ylabel('intensity')
+xlabel('phase shift EOM / pi')
+title('different positions')
+legend(legends,'Location','north')
 
-plot(sphases,squeeze(dangle),'r',sphases,squeeze(abs(vout(outchannel,:,:)).^2/2),'b',sphases,0*sphases,'k')
-legend('phase diff','intensity(0)')
-xlabel('phase shift')
-hold on
-plot(sphases,squeeze(dangle1),'r--',sphases,squeeze(abs(vout1(outchannel,:)).^2/2),'b--')
-plot(sphases,squeeze(dangle2),'r--',sphases,squeeze(abs(vout2(outchannel,:)).^2/2),'b--')
-title('EOM delay for 3 lateral phases')
-
-dp(1,1,:)=-2*pi:0.1:2*pi;
-[voutd, dangled]=EomSLM(vstart,-pi/4,dp);
-[voutd1, dangled1]=EomSLM(vstart,0,dp);
-[voutd2, dangled2]=EomSLM(vstart,pi/4,dp);
-sphases=squeeze(dp);
+clear phases dphi
+phases=-1/2:1/2:1; 
+dphi(1,1,:)=-1/2:1/32:1/2; %pi
 
 figure(88);subplot(1,2,2);hold off;
+sphi=squeeze(dphi);
+col=lines(length(dphi));
+legends={};
+for k=1:length(phases)
+    [vout, dangle]=EomSLM(vstart,phases(k)*pi,dphi*pi);
+    int0=squeeze(abs(vout(outchannel,:,:)).^2/2);
+    plot(sphi,int0,'Color',col(k,:))
+    [~,ind]=min(int0);
+    text(dphi(ind),0.03,num2str(phases(k)),"HorizontalAlignment","center","Color",'b')        
+    hold on
+    legends{end+1}=['phase = ' num2str(phases(k)) ' pi'];
+end
+text(min(dphi),0.03,'EOM phase',"HorizontalAlignment","right","Color",'b')
+plot(sphi,0*sphi,'k')
+title('different EOM phases')
+legend(legends,'Location','north')
+xlabel('lateral position / pi')
+ylabel('intensity')
 
-plot(sphases,squeeze(dangled),'r',sphases,squeeze(abs(voutd(outchannel,:,:)).^2/2),'b',sphases,0*sphases,'k')
-legend('phase diff','intensity(0)')
-xlabel('lateral position')
-hold on
-plot(sphases,squeeze(dangled1),'r--',sphases,squeeze(abs(voutd1(outchannel,:)).^2/2),'b--')
-plot(sphases,squeeze(dangled2),'r--',sphases,squeeze(abs(voutd2(outchannel,:)).^2/2),'b--')
 
-title('lateral phases for 3 EOM delays')
+ figure(89);hold off
+ [vout, dangle,vplot]=EomSLM(vstart,pi/2,0);
+ for k=1:length(vplot)
+     plotpolstate(vplot(k).state,2*k,0)
+     text(2*k-1,2,vplot(k).label)
+     hold on
+ end
+axis equal
+xlim([0 16])
 
-function [vout, dangle]=EomSLM(vstart,phases,dphi)
+
+function [vout, dangle,vplot]=EomSLM(vstart,phases,dphi) %EOM at 45°
 if nargin<3
     dphi=0;
 end
-%vphase=pagemtimes(phaseplate(phases),vstart);
-vphase=pagemtimes(QWP(phases),vstart);
-vphase=pagemtimes(addphase(dphi),vphase);
-vrot=pagemtimes(addphase(-dphi),pagemtimes(HWP45,vphase));
+vplot(1).state=vstart; vplot(1).label='in';
+vphase=pagemtimes(waveplate(phases,pi/4),vstart); %EOM 45  
+vplot(2).state=vphase; vplot(2).label='EOM 45°';
+vphase=pagemtimes(HWP(pi/8),vphase); %HWP
+vplot(3).state=vphase; vplot(3).label='HWP 22.5°';
+vrot=pagemtimes(addphase(-dphi),pagemtimes(HWP45,vphase)); %SLM+lat pos
+vplot(4).state=vrot; vplot(4).label='SLM+latpos';
+vphase=pagemtimes(addphase(dphi),vphase); %lat pos
+vplot(5).state=vphase; vplot(5).label='only latpos';
 vsum=(vrot+vphase);
-vout=pagemtimes(polarizer(0),vsum);
+vplot(6).state=vsum; vplot(6).label='sum';
+vout=pagemtimes(polarizer(0),vsum); %polarizer
+vplot(7).state=vout; vplot(7).label='polarizer';
 outchannel=1;
 dangle=mod(angle(vrot(outchannel,:))-angle(vphase(outchannel,:))+pi,2*pi)-pi;
 end
 
-function out=waveplate(phase,angle)
-out=exp(-1i*phase/2).*...
-    [cos(angle).^2+exp(1i*phase)*sin(angle).^2 (1-exp(1i*phase))*cos(angle).*sin(angle)
-    (1-exp(1i*phase))* cos(angle).*sin(angle) sin(angle).^2+exp(1i*phase)*cos(angle).^2];
+function out=waveplate(phaseretardation,rotationangle)
+out=exp(-1i*phaseretardation/2).*...
+    [cos(rotationangle).^2+exp(1i*phaseretardation)*sin(rotationangle).^2 (1-exp(1i*phaseretardation))*cos(rotationangle).*sin(rotationangle)
+    (1-exp(1i*phaseretardation))* cos(rotationangle).*sin(rotationangle) sin(rotationangle).^2+exp(1i*phaseretardation)*cos(rotationangle).^2]; %checked, correct
 end
 
-function out=QWP(angle)
-out=waveplate(pi/2,angle);
+function out=QWP(rotationangle)
+out=waveplate(pi/2,rotationangle);
 end
 function out=HWP(angle)
 out=waveplate(pi,angle);
@@ -70,7 +112,8 @@ function out=QWP45
 out=QWP(pi/4);
 end
 function out=phaseplate(phase)
-out=waveplate(2*phase,0);
+out=waveplate(phase,0); 
+% out=waveplate(2*phase,0);%Why factor of two?
 end
 function out=addphase(phase)
 out=exp(1i*phase);
@@ -78,10 +121,25 @@ end
 
 function out=linear(angle)
 v=[1;0];
-out=1i*HWP(angle/4)*v;
+out=1i*HWP(angle/2)*v; % I changed it to 2 so it matches, not tested.
 end
 
 function out=polarizer(angle)
 out=[cos(angle)^2 cos(angle)*sin(angle)
    cos(angle)*sin(angle) sin(angle)^2];
+end
+
+
+function plotpolstate(vin,dx,dy)
+dd=0.1;
+phases=0:dd:2*pi+dd;
+p1=angle(vin(1));
+p2=angle(vin(2));
+i1=abs(vin(1));
+i2=abs(vin(2));
+plot(i1*sin(phases+p1)+dx,i2*sin(phases+p2)+dy); hold on
+% 
+plot(i1*sin(phases(1)+p1)+dx,i2*sin(phases(1)+p2)+dy,'b*'); hold on
+plot(i1*sin(phases(2)+p1)+dx,i2*sin(phases(2)+p2)+dy,'bo'); hold on
+
 end
