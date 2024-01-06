b198620153f61ac17b581eb551cce8e34b68a29d
Jonas Ries <ries@embl.de>
2024-01-06 16:30:40 +0100

    cluster shuffling for any field

--- plugins/+Analyze/+cluster/ShuffelClusterIndex.m
+++ plugins/+Analyze/+cluster/ShuffelClusterIndex.m
@@ -8,16 +8,21 @@ classdef ShuffelClusterIndex<interfaces.DialogProcessor
             obj.history=true;
             obj.showresults=false;
         end
+
+        function initGuiFinal(obj)
+            locf=obj.getPar('locFields').String;
+            indg=find(strcmp(locf,'groupindex'));
+            if ~isempty(indg)
+                obj.guihandles.assignfield1.Value=indg;
+                obj.selectfield_callback;
+            end
+            
+        end
         function out=run(obj,p)
             out=[];
-            
-            if isfield(obj.locData.loc,'clusterindex')
-                field='clusterindex';
-            elseif isfield(obj.locData.loc,'track_id')
-                field='track_id';
-            else
-                disp('no clusterindex found');
-            end
+            field=obj.getSingleGuiParameter('assignfield1').selection;
+            outfield= obj.getSingleGuiParameter('resultfieldh');
+
             clusterindex=obj.locData.loc.(field);
             posind=clusterindex>0;
             
@@ -27,14 +32,33 @@ classdef ShuffelClusterIndex<interfaces.DialogProcessor
 %             indsort=vertcat(1,indsort);
             clusterindexnew=zeros(size(clusterindex),'single');
             clusterindexnew(posind) = indsort(clusterindex(posind));
-            obj.locData.loc.(field)=clusterindexnew;
+            obj.locData.setloc(outfield,clusterindexnew);
+            % obj.locData.loc.(outfield)=clusterindexnew;
             obj.locData.regroup;
+        end
+        function selectfield_callback(obj,a,b)
+            field=obj.getSingleGuiParameter('assignfield1').selection;
+            obj.setGuiParameters(struct('resultfieldh',[field '_rnd']));
         end
         
         function pardef=guidef(obj)
             pardef.plugininfo.name='ShuffleClusterIndices';
             pardef.plugininfo.description='ShuffelClusterIndex randomizes the index of clusters. This is useful for color-coded plotting of clusters.';
             pardef.plugininfo.type='ProcessorPlugin';
+
+            pardef.assignfield1.object=struct('Style','popupmenu','String','n1|n2','Callback',@obj.selectfield_callback);
+            pardef.assignfield1.position=[1,3.5];
+
+            pardef.resultfieldh.object=struct('String',{{''}},'Style','edit');
+            pardef.resultfieldh.position=[1,1];
+            pardef.resultfieldh.Width=1.5;
+
+            pardef.txt.object=struct('String',' = random shuffle of ','Style','text');
+            pardef.txt.position=[1,2.5];
+
+
+            pardef.syncParameters={{'locFields','assignfield1',{'String'}}};
+
         end
 
     end
