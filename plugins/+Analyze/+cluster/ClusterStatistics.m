a148c7d7cdd233f26f7dec56fcd797d4317007d8
Jonas Ries <jonas.ries@gmail.com>
2024-01-12 16:31:04 +0100

    add size analysis

--- plugins/+Analyze/+cluster/ClusterStatistics.m
+++ plugins/+Analyze/+cluster/ClusterStatistics.m
@@ -18,43 +18,89 @@ classdef ClusterStatistics<interfaces.DialogProcessor
         function out=run(obj,p)
            out=[];          
            layers=find(obj.getPar('sr_layerson'));
-           locs=obj.locData.getloc({'xnm','ynm','znm','clusterindex'},'layer',layers,'Position','roi');
-           clusterinds=unique(locs.clusterindex(locs.clusterindex>0));
+           locs=obj.locData.getloc({'xnm','ynm','znm','clusterindex','tid','groupindex'},'layer',layers,'Position','roi');
+           cfield=p.link.selection;
+           clusterinds=unique(locs.(cfield)(locs.(cfield)>0));
            cind=1;
-           ax=obj.initaxis('boundaries');
-           hold(ax,'off')
-           plot(ax,locs.xnm,locs.ynm,'b.');
+           if p.advanced
+               ax=obj.initaxis('boundaries');
+               hold(ax,'off')
+               plot(ax,locs.xnm,locs.ynm,'b.');
+           end
+           stdsall=zeros(size(obj.locData.loc.xnm),'single');
+           stdlall=stdsall;
+           angleall=stdsall;
+           exts=stdsall;
+           extl=stdsall;
            for k=1:length(clusterinds)
-               inc=locs.clusterindex==clusterinds(k);
-               if sum(inc)<p.minloc
+               inc=locs.(cfield)==clusterinds(k);
+               if sum(inc)<p.minloc || sum(inc)-2<=p.skipfirst
                    continue
                end
-               xh=double(locs.xnm(inc));yh=double(locs.ynm(inc));
+               
+
+               if p.skipfirst>0
+                   indo=find(inc,p.skipfirst,'first');
+                   inc(indo)=false;
+               end
+
                numlocs(cind,1)=sum(inc);
+
+               xh=double(locs.xnm(inc));yh=double(locs.ynm(inc));
+               
                xpos(cind,1)=mean(xh);
                ypos(cind,1)=mean(yh);
                
-               if ~isempty(locs.znm)
-                   zh=double(locs.znm(inc));
-                   zpos(cind,1)=mean(zh);
-                   [kc3,volumeCHull(cind,1)]=convhull(xh,yh,zh);
-                   [kb3,volumeB(cind,1)]=boundary(xh,yh,zh);
-               else
-                   volumeCHull(cind,1)=0;
-                   volumeB(cind,1)=0;
-                   zpos(cind,1)=0;
+               if p.advanced
+                   if ~isempty(locs.znm)
+                       zh=double(locs.znm(inc));
+                       zpos(cind,1)=mean(zh);
+                       [kc3,volumeCHull(cind,1)]=convhull(xh,yh,zh);
+                       [kb3,volumeB(cind,1)]=boundary(xh,yh,zh);
+                   else
+                       volumeCHull(cind,1)=0;
+                       volumeB(cind,1)=0;
+                       zpos(cind,1)=0;
+                   end
+                   [kc2,areaCHull(cind,1)]=convhull(xh,yh);
+                   [kb2,areaB(cind,1)]=boundary(xh,yh);
+                   cID(cind,1)=clusterinds(k);
+                   cind=cind+1;
+                   hold(ax,'on')
+                   plot(ax,xh,yh,'+')
+                   
+                   plot(ax,xh(kc2),yh(kc2),'r')
+                   plot(ax,xh(kb2),yh(kb2),'m')
                end
-               [kc2,areaCHull(cind,1)]=convhull(xh,yh);
-               [kb2,areaB(cind,1)]=boundary(xh,yh);
-               cID(cind,1)=clusterinds(k);
-               cind=cind+1;
-               hold(ax,'on')
-               plot(ax,xh,yh,'+')
+                
                
-               plot(ax,xh(kc2),yh(kc2),'r')
-               plot(ax,xh(kb2),yh(kb2),'m')
+               c = cov(xh-mean(xh), yh-mean(yh));
+               [a, ev] = eig(c);
+               [ev,ind] = sort(diag(ev));
+               [xa, ya] = deal(a(1,ind(end)), a(2,ind(end)));
+               angle = cart2pol(xa, ya);
+
+               indall=obj.locData.loc.(cfield)==clusterinds(k);
+               stdsall(indall)=sqrt(ev(1));
+               stdlall(indall)=sqrt(ev(2));
+               angleall(indall)=mod(angle,pi);
+               [xr,yr]=rotcoord(xh-mean(xh),yh-mean(yh),angle);
+               extl(indall)=max(xr)-min(xr);
+               exts(indall)=max(yr)-min(yr);
            end
-           obj.resultstable=table(cID,numlocs,areaCHull,volumeCHull,areaB,volumeB,xpos,ypos,zpos);
+           if p.advanced
+            obj.resultstable=table(cID,numlocs,areaCHull,volumeCHull,areaB,volumeB,xpos,ypos,zpos);
+           else
+           end
+           if p.addf
+               obj.locData.setloc('c_stds',stdsall)
+               obj.locData.setloc('c_stdl',stdlall)
+               obj.locData.setloc('c_angle',angleall)
+               obj.locData.setloc('c_extl',extl)
+               obj.locData.setloc('c_exts',exts)
+               obj.locData.regroup;
+           end
+
            
         end
         function pard=guidef(obj)
@@ -83,14 +129,37 @@ end
 
 function pard=guidef(obj)
 pard.minloct.object=struct('String','min number of locs per cluster','Style','text');
-pard.minloct.position=[1,1];
+pard.minloct.position=[3,1];
 pard.minloct.Width=2;
 pard.minloc.object=struct('String','10','Style','edit');
-pard.minloc.position=[1,3];
+pard.minloc.position=[3,2.5];
 pard.minloc.Width=0.5;
 
+
+pard.linkt.object=struct('String','Selection','Style','text');
+pard.linkt.position=[1,1];
+pard.link.object=struct('String',{{'clusterindex','groupindex','tid'}},'Style','popupmenu','Value',2);
+pard.link.position=[1,2];
+pard.link.Width=1.5;
+
+pard.advanced.object=struct('String','advanced analysis','Style','checkbox','Value',1);
+pard.advanced.position=[2,1];
+pard.advanced.Width=1.5;
+
+pard.addf.object=struct('String','add results to locs','Style','checkbox','Value',0);
+pard.addf.position=[2,3];
+pard.addf.Width=1.5;
+
+pard.skipt.object=struct('String','Skip first','Style','text');
+pard.skipt.position=[3,3];
+pard.skipfirst.object=struct('String',0,'Style','edit');
+pard.skipfirst.position=[3,4];
+pard.skipfirst.Width=0.5;
+
+
+
 pard.export.object=struct('String','export','Style','pushbutton','Callback',@obj.exportbutton);
-pard.export.position=[2,1];
+pard.export.position=[5,1];
 pard.export.Width=1;
 
 
