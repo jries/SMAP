3e99389f93e7ef03584f9b29a5432766180e4e9e
Jonas Ries <ries@a743-rie-mac2.msp.univie.ac.at>
2024-12-19 17:23:15 +0100

    dual-tracking analysis

--- plugins/+Analyze/+tracking/scripts/dual_track_analysis.m
+++ plugins/+Analyze/+tracking/scripts/dual_track_analysis.m
@@ -1,22 +1,29 @@
 % analyze dual-color tracks
 
 %filters
-minlenframes=3;
+minlenframes=5;
 maxd=200; % nm max distance to associate localizations
 
-cotracklength=2; %minimum data poits associated between tracks
-cotrackfraction = 0; %minimum fraction of localizations associated
+cotracklength=4; %minimum data poits associated between tracks
+cotrackfraction = 0.0; %minimum fraction of localizations associated
 
 % test directed movement
 aspectratio=1; %should be below this for directed motion.
-lennmstartend=150; % minimum endpoint- startpoint
-lennmmin=200; %minimum of largest extenstion
-
+lennmstartend=300; % minimum endpoint- startpoint
+lennmmin=0; %minimum of largest extenstion standard deviation (not so useful)
+minvelocity=200; %nm/s
+maxvelocity=1500;
 
 
 
 obj=g;
 [locs,indin]=obj.locData.getloc({'xnm','ynm','znm','frame','track_id','channel','track_length','layer','filenumber'},'layer',find(obj.getPar('sr_layerson')),'position','roi','grouping','ungrouped');
+
+
+exposuretime=obj.locData.files.file(locs.filenumber(1)).info.exposure;
+
+% XXX if exposure is wront, overwrite here
+% exposuretime= 10
 
 % test directed movement, calculate statistics
 usetracks=unique(locs.track_id(locs.track_id>0));
@@ -25,7 +32,7 @@ trackstat.stdshort=zeros(max(usetracks),1);
 trackstat.stdlong=zeros(max(usetracks),1);
 trackstat.lenframe=zeros(max(usetracks),1);
 trackstat.partnertrackid=zeros(max(usetracks),1);
-
+trackstat.velocity=zeros(max(usetracks),1);
 locs.track_length_new=zeros(size(locs.xnm));
 
 for k=1:length(usetracks)
@@ -44,12 +51,12 @@ for k=1:length(usetracks)
     c = cov(xh-mean(xh), yh-mean(yh));
     [a, ev] = eig(c);
     [ev,ind] = sort(diag(ev));
+    [xa, ya] = deal(a(1,ind(end)), a(2,ind(end)));
+    trackstat.angle(iduset)=cart2pol(xa, ya);
     trackstat.stdshort(iduset)=real(sqrt(ev(1)));
     trackstat.stdlong(iduset)=real(sqrt(ev(2)));
-
-
-               % [xa, ya] = deal(a(1,ind(end)), a(2,ind(end)));
-               % angle = cart2pol(xa, ya);          
+    trackstat.velocity(iduset)=trackstat.lennmstartend(iduset)/(exposuretime/1000*trackstat.lenframe(iduset)); %nm/s
+           
 end
 
 intrack=(locs.track_id>0);
@@ -90,80 +97,92 @@ partner(indr(iAa))=indt(iBa);
 partner(indt(iBa))=indr(iAa);
 
 usetracks=unique(locs.track_id(intrack&longtracks));
+
+roi=obj.locData.files.file(locs.filenumber(1)).info.roi;
+pixelsize=obj.locData.files.file(locs.filenumber(1)).info.cam_pixelsize_um*1000;
+
+
+%look for progressive movement
+validstats=true(size(trackstat.velocity));
+validstats = validstats & trackstat.stdshort./trackstat.stdlong<aspectratio;
+validstats = validstats & trackstat.lennmstartend > lennmstartend;
+validstats = validstats & trackstat.stdlong > lennmmin;
+validstats = validstats & trackstat.velocity > minvelocity;
+validstats = validstats & trackstat.velocity < maxvelocity;
+
+trackstat.progressive=validstats;
+
+comovement=true(size(trackstat.velocity));
+progressivepartner=false(size(trackstat.velocity));
+trackstat.channel=zeros(size(trackstat.velocity));
+
+
+for k=1:length(usetracks)  
+    idh=usetracks(k);
+    indtr=locs.track_id==idh;
+
+    %look at co-movement
+    pid=0;
+    comovement(idh)=trackstat.partnertrackid(idh)>0;
+    if comovement(idh) %&& validstats
+        partnerind=partner(indtr);
+        partnerids=locs.track_id(partnerind(partnerind>0));
+        [pid,npart]=mode(partnerids);
+        trackstat.partnerids(idh)=pid;
+        
+        lenpartner=sum(locs.track_id==pid);
+        minduallength=min(sum(indtr),lenpartner);
+
+        comovement(idh)=comovement(idh) & npart>cotracklength;
+        comovement(idh)=comovement(idh) & npart/minduallength>cotrackfraction;
+        progressivepartner(idh)=trackstat.progressive(pid);
+    end
+    trackstat.channel(idh)=mode(locs.channel(indtr));
+    
+end
+
+trackstat.comovement=comovement;
+trackstat.progressivepartner=progressivepartner;
+
+
+% plot tracks
 figure(88)
 hold off
 ax=gca;
-
 
 cols=[1 0 1
       0 1 1
       1 0 0 
       0 0 1
-      .5 0 .5
-      0 .5 .5];
+      .5 0.2 0.2
+      0.2 0.2 .5
+      .7 0 0.7
+      0 0.7 .7];
 
-roi=obj.locData.files.file(locs.filenumber(1)).info.roi;
-pixelsize=obj.locData.files.file(locs.filenumber(1)).info.cam_pixelsize_um*1000;
+colind=trackstat.channel+trackstat.comovement*2+(~trackstat.progressive)*4;   
 
-for k=1:length(usetracks)
+for k=1:length(usetracks)  
     idh=usetracks(k);
-    validcotrack=false;
-    validstats=true;
     indtr=locs.track_id==idh;
-    colind=mode(locs.channel(indtr));
     
+    msize=3;
     lw=1;
-    
-    %look at stats
-    validstats = validstats & trackstat.stdshort(idh)/trackstat.stdlong(idh)<aspectratio;
-    validstats = validstats & trackstat.lennmstartend(idh) > lennmstartend;
-    validstats = validstats & trackstat.stdlong(idh) > lennmmin;
+    symb='-';
 
-    
-
-
-    %look at co-movement
-    pid=0;
-    if trackstat.partnertrackid(idh)>0 %&& validstats
-        partnerind=partner(indtr);
-        partnerids=locs.track_id(partnerind(partnerind>0));
-        [pid,npart]=mode(partnerids);
-        lenpartner=sum(locs.track_id==pid);
-        minduallength=min(sum(indtr),lenpartner);
-        if npart>cotracklength && npart/minduallength>cotrackfraction
-            validcotrack=true;
-            lw=6;
-            colind=colind+2;
-            colind=colind+(~validstats)*2;
+    if trackstat.comovement(idh)
+        if trackstat.channel(idh)==2
+                symb='x-';
+                msize=3;
+        else
+                symb='+-';
+                msize=7;
+                lw=2;            
         end
     end
 
-    
-
-    % if any(pairedr(indtr)) 
-    %     partnerind=partnerr(indtr);
-    % elseif any(pairedt(indtr)) 
-    %     partnerind=partnert(indtr);
-    % end
-    % 
-    % if (any(pairedr(indtr)) || any(pairedt(indtr))) && validstats
-    %     partnerids=locs.track_id(partnerind(partnerind>0));
-    %     [pid,npart]=mode(partnerids);
-    %     if npart>cotracklength && npart/sum(indtr)>cotrackfraction
-    %         validcotrack=true;
-    %         lw=6;
-    %         colind=colind+2;
-    %         colind=colind+(~validstats)*2;
-    %     end
-    % end
-    
-    hp=plot(ax,locs.xnm(indtr)/pixelsize(1)-roi(1),locs.ynm(indtr)/pixelsize(2)-roi(2),'.-','Color',cols(colind,:),'LineWidth',lw,'Tag','test');
-      % hp=plot(ax,locs.xnm(indtr),locs.ynm(indtr),'.-','Color',cols(colind,:),'LineWidth',lw,'Tag','test');
+    hp=plot(ax,locs.xnm(indtr)/pixelsize(1)-roi(1),locs.ynm(indtr)/pixelsize(2)-roi(2),symb,'Color',cols(colind(idh),:),'LineWidth',lw,'Tag','test','MarkerSize',msize);
 
     hold(ax,"on")
-    % hp.DataTipTemplate.DataTipRows(1).Label = "X";
-    % hp.DataTipTemplate.DataTipRows(2).Label = "Y"; 
-    % hp.DataTipTemplate.DataTipRows(3).Label = "frame"; 
         pidlabel=0*locs.track_id(indtr)+pid;
 
         dtRows = [dataTipTextRow("frame",locs.frame(indtr)),...
@@ -177,4 +196,6 @@ end
     axis(ax,'ij');
     axis(ax,'equal');
 
-% Plot surviving tracks
+% Plot cotracks vs time
+
+% Calculate statistics
