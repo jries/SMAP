95d1b61a4c0bb1f8f9d7ec63cd410b7981c7d1e4
Jonas Ries <ries@embl.de>
2026-08-15 23:56:40 +0200

    overlay for alternating

--- plugins/+Analyze/+tracking/scripts/dual_movie_overlay.m
+++ plugins/+Analyze/+tracking/scripts/dual_movie_overlay.m
@@ -1,15 +1,17 @@
 %overlay dual color tracking images
 
-obj=g;
-datafile='/Users/ries/datalocal/2color_kinesin/25_50ms_561nm01_640nm02_600w52_676w37_1_MMStack_Default.ome.tif';
-Tfile='/Users/ries/datalocal/2color_kinesin/75_50ms_561nm01_640nm02_600w52_676w37_1_T.mat';
+%obj=g;
+datafile='/Users/ries/Data_local/temp/kinesins/614_purified_DualChannel_488i3_640i5_Alternate_3/614_purified_DualChannel_488i3_640i5_Alternate_MMStack_Pos0.ome.tif';
+Tfile='/Users/ries/Data_local/temp/kinesins/Bead_Cal_488_640_lessIntensityLaser_T.mat';
+
+alternating=true;
 
 tt=load(Tfile).transformation;
 il=imageloaderMM; il.attachPar(g.P);
 il.openi(datafile);
 il.prefit;
 
-numf=il.metadata.numberOfFrames+1;
+numf=il.metadata.numberOfFrames;
 
 sx=ceil(il.metadata.Width/2);
 imcomb=zeros(il.metadata.Height,sx,3,numf);
@@ -18,13 +20,23 @@ imcomb=zeros(il.metadata.Height,sx,3,numf);
 for k=1:numf
     img=double(il.getimage(k));
     imgt=tt.transformImageToTarget(2,img,'pixel',il.metadata.roi);
-    imcomb(:,:,1,k)=img(:,1:sx);
+    imcomb(:,:,2,k)=img(:,1:sx);
     % imcomb(:,:,3,k)=img(:,1:sx);
-    imcomb(:,:,2,k)=imgt(:,1:sx);
+    imcomb(:,:,1,k)=imgt(:,1:sx);
     
 end
 
 ih=imx(imcomb);
+
+if alternating
+    inodd=1:2:numf;
+    ineven=2:2:numf;
+    im1=imcomb(:,:,1,inodd);
+    im2=imcomb(:,:,2,ineven);
+    im3=imcomb(:,:,3,inodd);
+    imcomb=zeros(size(imcomb,1),size(imcomb,2),3,ceil(numf/2));
+    imcomb(:,:,1,:)=im1;imcomb(:,:,2,:)=im2;imcomb(:,:,3,:)=im3;
+end
 
 fout=strrep(datafile,'.ome.tif','_combined.tif')
 options.color=true;
